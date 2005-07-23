e4cbfc879930c694fb0a87ce70e1d35b379f0912
Simon Howard <fraggle@gmail.com>
2005-07-23 16:19:41 +0000

    Initial revision

---
+++ src/m_misc.h
@@ -0,0 +1,68 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
+// This source is available for distribution and/or modification
+// only under the terms of the DOOM Source Code License as
+// published by id Software. All rights reserved.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
+// for more details.
+//
+// DESCRIPTION:
+//
+//    
+//-----------------------------------------------------------------------------
+
+
+#ifndef __M_MISC__
+#define __M_MISC__
+
+
+#include "doomtype.h"
+//
+// MISC
+//
+
+
+
+boolean
+M_WriteFile
+( char const*	name,
+  void*		source,
+  int		length );
+
+int
+M_ReadFile
+( char const*	name,
+  byte**	buffer );
+
+void M_ScreenShot (void);
+
+void M_LoadDefaults (void);
+
+void M_SaveDefaults (void);
+
+
+int
+M_DrawText
+( int		x,
+  int		y,
+  boolean	direct,
+  char*		string );
+
+
+#endif
+//-----------------------------------------------------------------------------
+//
+// $Log$
+// Revision 1.1  2005/07/23 16:19:54  fraggle
+// Initial revision
+//
+//
+//-----------------------------------------------------------------------------
---
+++ src/r_sky.c
@@ -0,0 +1,65 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright (C) 1993-1996 by id Software, Inc.
+//
+// This source is available for distribution and/or modification
+// only under the terms of the DOOM Source Code License as
+// published by id Software. All rights reserved.
+//
+// The source is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
+// for more details.
+//
+// $Log$
+// Revision 1.1  2005/07/23 16:19:43  fraggle
+// Initial revision
+//
+//
+// DESCRIPTION:
+//  Sky rendering. The DOOM sky is a texture map like any
+//  wall, wrapping around. A 1024 columns equal 360 degrees.
+//  The default sky map is 256 columns and repeats 4 times
+//  on a 320 screen?
+//  
+//
+//-----------------------------------------------------------------------------
+
+static const char
+rcsid[] = "$Id$";
+
+
+// Needed for FRACUNIT.
+#include "m_fixed.h"
+
+// Needed for Flat retrieval.
+#include "r_data.h"
+
+
+#ifdef __GNUG__
+#pragma implementation "r_sky.h"
+#endif
+#include "r_sky.h"
+
+//
+// sky mapping
+//
+int			skyflatnum;
+int			skytexture;
+int			skytexturemid;
+
+
+
+//
+// R_InitSkyMap
+// Called whenever the view size changes.
+//
+void R_InitSkyMap (void)
+{
+  // skyflatnum = R_FlatNumForName ( SKYFLATNAME );
+    skytexturemid = 100*FRACUNIT;
+}
+
