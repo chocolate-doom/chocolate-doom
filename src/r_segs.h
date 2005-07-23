9de3d1409ac337b050082a983b72959f7f0551fb
Simon Howard <fraggle@gmail.com>
2005-07-23 16:44:57 +0000

    Update copyright to GNU GPL

--- src/r_segs.h
+++ src/r_segs.h
@@ -3,16 +3,23 @@
 //
 // $Id$
 //
-// Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 2005 Simon Howard
 //
-// This source is available for distribution and/or modification
-// only under the terms of the DOOM Source Code License as
-// published by id Software. All rights reserved.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-// The source is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
-// for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
 //
 // DESCRIPTION:
 //	Refresh module, drawing LineSegs from BSP.
@@ -40,8 +47,11 @@ R_RenderMaskedSegRange
 //-----------------------------------------------------------------------------
 //
 // $Log$
-// Revision 1.1  2005/07/23 16:20:01  fraggle
-// Initial revision
+// Revision 1.2  2005/07/23 16:44:56  fraggle
+// Update copyright to GNU GPL
+//
+// Revision 1.1.1.1  2005/07/23 16:20:01  fraggle
+// Initial import
 //
 //
 //-----------------------------------------------------------------------------
