7f0cb76e0a82f8de0c40fdbca84b0c51962ec40b
Simon Howard <fraggle@gmail.com>
2005-07-23 16:44:57 +0000

    Update copyright to GNU GPL
    Subversion-branch: /trunk/chocolate-doom
    Subversion-revision: 8

--- src/d_think.h
+++ src/d_think.h
@@ -1,18 +1,25 @@
 // Emacs style mode select   -*- C++ -*- 
 //-----------------------------------------------------------------------------
 //
-// $Id: d_think.h 4 2005-07-23 16:19:41Z fraggle $
+// $Id: d_think.h 8 2005-07-23 16:44:57Z fraggle $
 //
-// Copyright (C) 1993-1996 by id Software, Inc.
+// Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 2005 Simon Howard
 //
-// This source is available for distribution and/or modification
-// only under the terms of the DOOM Source Code License as
-// published by id Software. All rights reserved.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-// The source is distributed in the hope that it will be useful,
+// This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// FITNESS FOR A PARTICULAR PURPOSE. See the DOOM Source Code License
-// for more details.
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
 //
 // DESCRIPTION:
 //  MapObj data. Map Objects or mobjs are actors, entities,
@@ -75,8 +82,11 @@ typedef struct thinker_s
 //-----------------------------------------------------------------------------
 //
 // $Log$
-// Revision 1.1  2005/07/23 16:19:44  fraggle
-// Initial revision
+// Revision 1.2  2005/07/23 16:44:55  fraggle
+// Update copyright to GNU GPL
+//
+// Revision 1.1.1.1  2005/07/23 16:19:44  fraggle
+// Initial import
 //
 //
 //-----------------------------------------------------------------------------
