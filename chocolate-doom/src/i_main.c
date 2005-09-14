2e30841eb24c438f040f4af3030400bd8a48ccfd
Simon Howard <fraggle@gmail.com>
2005-09-14 22:08:29 +0000

    Fix startup messages displayed (build a console binary; remove CON redirection code)

--- chocolate-doom/src/i_main.c
+++ chocolate-doom/src/i_main.c
@@ -22,6 +22,10 @@
 // 02111-1307, USA.
 //
 // $Log$
+// Revision 1.6  2005/09/14 22:08:28  fraggle
+// Fix startup messages displayed (build a console binary; remove CON
+// redirection code)
+//
 // Revision 1.5  2005/09/07 22:58:34  fraggle
 // No SIGHUP on Windows
 //
@@ -80,14 +84,6 @@ int main(int argc, char **argv)
     signal(SIGHUP, SignalHandler);
 #endif
 
-#ifdef _WIN32
-    // restore stdout/stderr
-
-    freopen("CON", "w", stdout);
-    freopen("CON", "w", stderr);
-
-#endif
-    
     // start doom
  
     D_DoomMain (); 
