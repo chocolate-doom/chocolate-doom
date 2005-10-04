9aa73aa6fb9932abc0da89efd9d30d67338928e5
Simon Howard <fraggle@gmail.com>
2005-10-04 22:10:32 +0000

    Dehacked "Misc" section parser (currently a dummy)

---
+++ chocolate-doom/src/deh_misc.c
@@ -0,0 +1,57 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright(C) 2005 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+// $Log$
+// Revision 1.1  2005/10/04 22:10:32  fraggle
+// Dehacked "Misc" section parser (currently a dummy)
+//
+//
+//-----------------------------------------------------------------------------
+//
+// Parses "Misc" sections in dehacked files
+//
+//-----------------------------------------------------------------------------
+
+#include "doomdef.h"
+#include "doomtype.h"
+#include "deh_defs.h"
+
+static void *DEH_MiscStart(deh_context_t *context, char *line)
+{
+    fprintf(stderr, "DEH_MiscStart: Warning: dehacked 'Misc' sections are "
+                    "not yet supported.\n");
+    return NULL;
+}
+
+static void DEH_MiscParseLine(deh_context_t *context, char *line, void *tag)
+{
+}
+
+deh_section_t deh_section_misc =
+{
+    "Misc",
+    NULL,
+    DEH_MiscStart,
+    DEH_MiscParseLine,
+    NULL,
+};
+
