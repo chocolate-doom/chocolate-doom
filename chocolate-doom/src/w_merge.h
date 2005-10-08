9d84832922e66213f7539346d1c9a0411e3f684e
Simon Howard <fraggle@gmail.com>
2005-10-08 18:23:18 +0000

    WAD merging code

---
+++ chocolate-doom/src/w_merge.h
@@ -0,0 +1,44 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id$
+//
+// Copyright(C) 2005 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+// $Log$
+// Revision 1.1  2005/10/08 18:23:18  fraggle
+// WAD merging code
+//
+//
+// DESCRIPTION:
+// Handles merging of PWADs, similar to deutex's -merge option
+//
+// Ideally this should work exactly the same as in deutex, but trying to
+// read the deutex source code made my brain hurt.
+//
+//-----------------------------------------------------------------------------
+
+#ifndef W_MERGE_H
+#define W_MERGE_H
+
+// Add a new WAD and merge it into the main directory
+
+void W_MergeFile(char *filename);
+
+#endif /* #ifndef W_MERGE_H */
+
