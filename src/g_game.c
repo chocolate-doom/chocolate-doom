2feee47c1a7b86757b05ebe715392d436368133e
Simon Howard <fraggle@gmail.com>
2006-01-23 00:12:36 +0000

    Fix dehacked sky replacement

--- src/g_game.c
+++ src/g_game.c
@@ -22,6 +22,9 @@
 // 02111-1307, USA.
 //
 // $Log$
+// Revision 1.20.2.2  2006/01/23 00:12:36  fraggle
+// Fix dehacked sky replacement
+//
 // Revision 1.20.2.1  2006/01/22 23:36:21  fraggle
 // Allow changing the sky texture names via dehacked patches
 //
@@ -596,6 +599,8 @@ void G_DoLoadLevel (void)
 	    break;
 	} 
     }
+
+    skytexturename = DEH_String(skytexturename);
 
     skytexture = R_TextureNumForName(skytexturename);
 
