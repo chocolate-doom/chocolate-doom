da44898ed8665f97913e6c0cec94469a3ae87ace
Simon Howard <fraggle@gmail.com>
2006-02-27 17:57:39 +0000

    Reduce the maximum latency to stop the game pausing and shooting ahead.

--- src/d_net.c
+++ src/d_net.c
@@ -224,9 +224,9 @@ void NetUpdate (void)
         if ((!netgame || demoplayback) && maketic - gameticdiv > 2)
             break;
 
-        // Never go more than a second ahead
+        // Never go more than ~200ms ahead
 
-        if (maketic - gameticdiv > 35)
+        if (maketic - gameticdiv > 8)
             break;
 
 	//printf ("mk:%i ",maketic);
