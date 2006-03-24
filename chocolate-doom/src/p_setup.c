1540aa141e6e018ac3d83b26c8d560bee8fedd4c
Simon Howard <fraggle@gmail.com>
2006-03-24 16:51:28 +0000

    Use "sizeof(line_t *)" not "4".  Fixes 64-bit builds.

--- chocolate-doom/src/p_setup.c
+++ chocolate-doom/src/p_setup.c
@@ -556,7 +556,7 @@ void P_GroupLines (void)
     }
 	
     // build line tables for each sector	
-    linebuffer = Z_Malloc (total*4, PU_LEVEL, 0);
+    linebuffer = Z_Malloc (total*sizeof(line_t *), PU_LEVEL, 0);
     sector = sectors;
     for (i=0 ; i<numsectors ; i++, sector++)
     {
