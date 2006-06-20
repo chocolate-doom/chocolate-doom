decc0a876f8fa1c3cd8fef6307809c6617933b73
Simon Howard <fraggle@gmail.com>
2006-06-20 18:49:36 +0000

    Add a fake nonfunctional key bindings configuration menu.
    Subversion-branch: /trunk/chocolate-doom
    Subversion-revision: 561

--- setup/keyboard.c
+++ setup/keyboard.c
@@ -9,8 +9,11 @@ void ConfigKeyboard(void)
 
     window = TXT_NewWindow("Keyboard configuration");
 
+    TXT_AddWidget(window, TXT_NewSeparator("Movement"));
+
     table = TXT_NewTable(2);
-    TXT_AddWidget(window, table);
+    TXT_AddWidget(table, TXT_NewStrut(20, 0));
+    TXT_AddWidget(table, TXT_NewStrut(8, 0));
 
     TXT_AddWidget(table, TXT_NewLabel("Move Forward"));
     TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
@@ -19,5 +22,29 @@ void ConfigKeyboard(void)
     TXT_AddWidget(table, TXT_NewLabel("Turn Left"));
     TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
     TXT_AddWidget(table, TXT_NewLabel("Turn Right"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+    TXT_AddWidget(table, TXT_NewLabel("Strafe Left"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+    TXT_AddWidget(table, TXT_NewLabel("Strafe Right"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+    TXT_AddWidget(table, TXT_NewLabel("Speed On"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+    TXT_AddWidget(table, TXT_NewLabel("Strafe On"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+
+    TXT_AddWidget(window, table);
+
+    TXT_AddWidget(window, TXT_NewSeparator("Action"));
+
+    table = TXT_NewTable(2);
+    TXT_AddWidget(table, TXT_NewStrut(20, 0));
+    TXT_AddWidget(table, TXT_NewStrut(8, 0));
+
+    TXT_AddWidget(table, TXT_NewLabel("Use"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+    TXT_AddWidget(table, TXT_NewLabel("Fire"));
+    TXT_AddWidget(table, TXT_NewIntInputBox(&dummy, 7));
+
+    TXT_AddWidget(window, table);
 }
 
