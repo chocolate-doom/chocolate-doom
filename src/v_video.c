cdc3b679daba3c724b588389aaabcd4753effa38
Simon Howard <fraggle@gmail.com>
2006-08-06 22:51:29 +0000

    Bomb out with an error when trying to draw patches that go off the screen, as Vanilla Doom does.

--- src/v_video.c
+++ src/v_video.c
@@ -244,10 +244,7 @@ V_DrawPatch
 	|| y+SHORT(patch->height)>SCREENHEIGHT 
 	|| (unsigned)scrn>4)
     {
-      fprintf( stderr, "Patch at %d,%d exceeds LFB\n", x,y );
-      // No I_Error abort - what is up with TNT.WAD?
-      fprintf( stderr, "V_DrawPatch: bad patch (ignored)\n");
-      return;
+        I_Error("Bad V_DrawPatch");
     }
 #endif 
  
@@ -311,8 +308,7 @@ V_DrawPatchFlipped
 	|| y+SHORT(patch->height)>SCREENHEIGHT 
 	|| (unsigned)scrn>4)
     {
-      fprintf( stderr, "Patch origin %d,%d exceeds LFB\n", x,y );
-      I_Error ("Bad V_DrawPatch in V_DrawPatchFlipped");
+        I_Error("Bad V_DrawPatchFlipped");
     }
 #endif 
  
