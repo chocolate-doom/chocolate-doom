dd3489015169ec0591dfb212cace2f2626678031
Simon Howard <fraggle@gmail.com>
2006-10-05 18:55:07 +0000

    WAD directory checksum generation code

---
+++ src/w_checksum.h
@@ -0,0 +1,36 @@
+// Emacs style mode select   -*- C++ -*- 
+//-----------------------------------------------------------------------------
+//
+// $Id: w_wad.h 596 2006-09-02 19:10:07Z fraggle $
+//
+// Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 2005 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+// DESCRIPTION:
+//       Generate a checksum of the WAD directory.
+//
+
+#ifndef W_CHECKSUM_H
+#define W_CHECKSUM_H
+
+#include "doomtype.h"
+
+extern void W_Checksum(byte digest[16]);
+
+#endif /* #ifndef W_CHECKSUM_H */
+
