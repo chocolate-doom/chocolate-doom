b50d11659b7596a4b4d9fecfc9427b6569975ef3
Simon Howard <fraggle@gmail.com>
2007-02-01 12:21:14 +0000

    Search IWAD search directories when loading PWAD files.

--- chocolate-doom/src/d_main.c
+++ chocolate-doom/src/d_main.c
@@ -954,8 +954,12 @@ void D_DoomMain (void)
     {
         for (p = p + 1; p<myargc && myargv[p][0] != '-'; ++p)
         {
-            printf(" merging %s\n", myargv[p]);
-            W_MergeFile(myargv[p]);
+            char *filename;
+
+            filename = D_TryFindWADByName(myargv[p]);
+
+            printf(" merging %s\n", filename);
+            W_MergeFile(filename);
         }
     }
 
@@ -976,8 +980,12 @@ void D_DoomMain (void)
     {
         for (p = p + 1; p<myargc && myargv[p][0] != '-'; ++p)
         {
-            printf(" performing NWT-style merge of %s\n", myargv[p]);
-            W_NWTDashMerge(myargv[p]);
+            char *filename;
+
+            filename = D_TryFindWADByName(myargv[p]);
+
+            printf(" performing NWT-style merge of %s\n", filename);
+            W_NWTDashMerge(filename);
         }
     }
     
@@ -997,8 +1005,12 @@ void D_DoomMain (void)
     {
         for (p = p + 1; p<myargc && myargv[p][0] != '-'; ++p)
         {
-            printf(" merging flats from %s\n", myargv[p]);
-            W_NWTMergeFile(myargv[p], W_NWT_MERGE_FLATS);
+            char *filename;
+
+            filename = D_TryFindWADByName(myargv[p]);
+
+            printf(" merging flats from %s\n", filename);
+            W_NWTMergeFile(filename, W_NWT_MERGE_FLATS);
         }
     }
 
@@ -1016,8 +1028,12 @@ void D_DoomMain (void)
     {
         for (p = p + 1; p<myargc && myargv[p][0] != '-'; ++p)
         {
-            printf(" merging sprites from %s\n", myargv[p]);
-            W_NWTMergeFile(myargv[p], W_NWT_MERGE_SPRITES);
+            char *filename;
+
+            filename = D_TryFindWADByName(myargv[p]);
+
+            printf(" merging sprites from %s\n", filename);
+            W_NWTMergeFile(filename, W_NWT_MERGE_SPRITES);
         }
     }
 
@@ -1034,8 +1050,12 @@ void D_DoomMain (void)
     {
         for (p = p + 1; p<myargc && myargv[p][0] != '-'; ++p)
         {
-            printf(" merging sprites and flats from %s\n", myargv[p]);
-            W_NWTMergeFile(myargv[p], W_NWT_MERGE_SPRITES | W_NWT_MERGE_FLATS);
+            char *filename;
+
+            filename = D_TryFindWADByName(myargv[p]);
+
+            printf(" merging sprites and flats from %s\n", filename);
+            W_NWTMergeFile(filename, W_NWT_MERGE_SPRITES | W_NWT_MERGE_FLATS);
         }
     }
 
@@ -1054,7 +1074,13 @@ void D_DoomMain (void)
 	// until end of parms or another - preceded parm
 	modifiedgame = true;            // homebrew levels
 	while (++p != myargc && myargv[p][0] != '-')
-	    D_AddFile (myargv[p]);
+        {
+            char *filename;
+
+            filename = D_TryFindWADByName(myargv[p]);
+
+	    D_AddFile(filename);
+        }
     }
 
     // Debug:
