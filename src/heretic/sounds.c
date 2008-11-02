20f25c9e9714dd2f81a34acc22fb0986fcff0cb3
Simon Howard <fraggle@gmail.com>
2008-11-02 23:53:36 +0000

    Fix up raven-branch copyright notices.

--- src/heretic/sounds.c
+++ src/heretic/sounds.c
@@ -3,6 +3,7 @@
 //
 // Copyright(C) 1993-1996 Id Software, Inc.
 // Copyright(C) 1993-2008 Raven Software
+// Copyright(C) 2008 Simon Howard
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
--- src/v_video.c
+++ src/v_video.c
@@ -2,6 +2,7 @@
 //-----------------------------------------------------------------------------
 //
 // Copyright(C) 1993-1996 Id Software, Inc.
+// Copyright(C) 1993-2008 Raven Software
 // Copyright(C) 2005 Simon Howard
 //
 // This program is free software; you can redistribute it and/or
