b282b1a313e83c2fa848975da3df6e332bbb151a
Simon Howard <fraggle@gmail.com>
2008-12-01 22:13:53 +0000

    Add missing header includes.

--- src/i_sdlsound.c
+++ src/i_sdlsound.c
@@ -30,6 +30,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 #include "SDL.h"
 #include "SDL_mixer.h"
--- src/i_video.c
+++ src/i_video.c
@@ -29,6 +29,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
+#include <string.h>
 
 #include "icon.c"
 
