39b7cb7bb2e14169af5dc07c7d429fc939200639
Simon Howard <fraggle@gmail.com>
2009-01-30 23:53:47 +0000

    Fix layout of widgets within scroll panes. Scroll scroll panes in response to keyboard events.
    Subversion-branch: /trunk/chocolate-doom
    Subversion-revision: 1440

--- textscreen/txt_table.h
+++ textscreen/txt_table.h
@@ -46,9 +46,12 @@ struct txt_table_s
     int selected_y;
 };
 
+extern txt_widget_class_t txt_table_class;
+
 txt_table_t *TXT_NewTable(int columns);
 txt_table_t *TXT_NewHorizBox(TXT_UNCAST_ARG(first_widget), ...);
 void TXT_InitTable(txt_table_t *table, int columns);
+txt_widget_t *TXT_GetSelectedWidget(TXT_UNCAST_ARG(table));
 void TXT_AddWidget(TXT_UNCAST_ARG(table), TXT_UNCAST_ARG(widget));
 void TXT_AddWidgets(TXT_UNCAST_ARG(table), ...);
 int TXT_SelectWidget(TXT_UNCAST_ARG(table), TXT_UNCAST_ARG(widget));
