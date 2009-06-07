d886fc06dcbf4151d697d05ff9c601cd801349c3
Simon Howard <fraggle@gmail.com>
2009-06-07 02:10:21 +0000

    Use MessageBoxW instead of MessageBox (doesn't exist on Windows CE)

--- src/i_system.c
+++ src/i_system.c
@@ -246,13 +246,18 @@ void I_Error (char *error, ...)
     // On Windows, pop up a dialog box with the error message.
     {
         char msgbuf[512];
+        wchar_t wmsgbuf[512];
 
         va_start(argptr, error);
         memset(msgbuf, 0, sizeof(msgbuf));
         vsnprintf(msgbuf, sizeof(msgbuf) - 1, error, argptr);
         va_end(argptr);
 
-        MessageBox(NULL, msgbuf, "Error", MB_OK);
+        MultiByteToWideChar(CP_ACP, 0,
+                            msgbuf, strlen(msgbuf) + 1,
+                            wmsgbuf, sizeof(wmsgbuf));
+
+        MessageBoxW(NULL, wmsgbuf, L"Error", MB_OK);
     }
 #endif
 
