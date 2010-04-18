72afdec76ec4ab7c84379223eb1179e18cf1367c
Simon Howard <fraggle@gmail.com>
2010-04-18 18:59:17 +0000

    Remove unused strings.
    Subversion-branch: /branches/raven-branch
    Subversion-revision: 1908

--- src/heretic/dstrings.h
+++ src/heretic/dstrings.h
@@ -26,42 +26,6 @@
 
 //---------------------------------------------------------------------------
 //
-// M_menu.c
-//
-//---------------------------------------------------------------------------
-#define PRESSKEY 	"press a key."
-#define PRESSYN 	"press y or n."
-#define TXT_PAUSED "PAUSED"
-#define QUITMSG		"are you sure you want to\nquit this great game?"
-#define LOADNET 	"you can't do load while in a net game!\n\n"PRESSKEY
-#define QLOADNET	"you can't quickload during a netgame!\n\n"PRESSKEY
-#define QSAVESPOT	"you haven't picked a quicksave slot yet!\n\n"PRESSKEY
-#define SAVEDEAD 	"you can't save if you aren't playing!\n\n"PRESSKEY
-#define QSPROMPT 	"quicksave over your game named\n\n'%s'?\n\n"PRESSYN
-#define QLPROMPT	"do you want to quickload the game named"\
-					"\n\n'%s'?\n\n"PRESSYN
-#define NEWGAME		"you can't start a new game\n"\
-					"while in a network game.\n\n"PRESSKEY
-#define NIGHTMARE	"are you sure? this skill level\n"\
-					"isn't even remotely fair.\n\n"PRESSYN
-#define SWSTRING	"this is the shareware version of doom.\n\n"\
-					"you need to order the entire trilogy.\n\n"PRESSKEY
-#define MSGOFF		"Messages OFF"
-#define MSGON		"Messages ON"
-#define NETEND		"you can't end a netgame!\n\n"PRESSKEY
-#define ENDGAME		"are you sure you want to end the game?\n\n"PRESSYN
-#define DOSY		"(press y to quit to dos.)"
-#define DETAILHI	"High detail"
-#define DETAILLO	"Low detail"
-#define GAMMALVL0	"Gamma correction OFF"
-#define GAMMALVL1	"Gamma correction level 1"
-#define GAMMALVL2	"Gamma correction level 2"
-#define GAMMALVL3	"Gamma correction level 3"
-#define GAMMALVL4	"Gamma correction level 4"
-#define	EMPTYSTRING	"empty slot"
-
-//---------------------------------------------------------------------------
-//
 // P_inter.c
 //
 //---------------------------------------------------------------------------
@@ -170,74 +134,6 @@
 
 //---------------------------------------------------------------------------
 //
-// HU_stuff.c
-//
-//---------------------------------------------------------------------------
-
-#define HUSTR_E1M1	"E1M1: Hangar"
-#define HUSTR_E1M2	"E1M2: Nuclear Plant"
-#define HUSTR_E1M3	"E1M3: Toxin Refinery"
-#define HUSTR_E1M4	"E1M4: Command Control"
-#define HUSTR_E1M5	"E1M5: Phobos Lab"
-#define HUSTR_E1M6	"E1M6: Central Processing"
-#define HUSTR_E1M7	"E1M7: Computer Station"
-#define HUSTR_E1M8	"E1M8: Phobos Anomaly"
-#define HUSTR_E1M9	"E1M9: Military Base"
-
-#define HUSTR_E2M1	"E2M1: Deimos Anomaly"
-#define HUSTR_E2M2	"E2M2: Containment Area"
-#define HUSTR_E2M3	"E2M3: Refinery"
-#define HUSTR_E2M4	"E2M4: Deimos Lab"
-#define HUSTR_E2M5	"E2M5: Command Center"
-#define HUSTR_E2M6	"E2M6: Halls of the Damned"
-#define HUSTR_E2M7	"E2M7: Spawning Vats"
-#define HUSTR_E2M8	"E2M8: Tower of Babel"
-#define HUSTR_E2M9	"E2M9: Fortress of Mystery"
-
-#define HUSTR_E3M1	"E3M1: Hell Keep"
-#define HUSTR_E3M2	"E3M2: Slough of Despair"
-#define HUSTR_E3M3	"E3M3: Pandemonium"
-#define HUSTR_E3M4	"E3M4: House of Pain"
-#define HUSTR_E3M5	"E3M5: Unholy Cathedral"
-#define HUSTR_E3M6	"E3M6: Mt. Erebus"
-#define HUSTR_E3M7	"E3M7: Limbo"
-#define HUSTR_E3M8	"E3M8: Dis"
-#define HUSTR_E3M9	"E3M9: Warrens"
-
-#define HUSTR_CHATMACRO1	"I'm ready to kick butt!"
-#define HUSTR_CHATMACRO2	"I'm OK."
-#define HUSTR_CHATMACRO3	"I'm not looking too good!"
-#define HUSTR_CHATMACRO4	"Help!"
-#define HUSTR_CHATMACRO5	"You suck!"
-#define HUSTR_CHATMACRO6	"Next time, scumbag..."
-#define HUSTR_CHATMACRO7	"Come here!"
-#define HUSTR_CHATMACRO8	"I'll take care of it."
-#define HUSTR_CHATMACRO9	"Yes"
-#define HUSTR_CHATMACRO0	"No"
-
-#define HUSTR_TALKTOSELF1	"You mumble to yourself"
-#define HUSTR_TALKTOSELF2	"Who's there?"
-#define HUSTR_TALKTOSELF3	"You scare yourself"
-#define HUSTR_TALKTOSELF4	"You start to rave"
-#define HUSTR_TALKTOSELF5	"You've lost it..."
-
-#define HUSTR_MESSAGESENT	"[Message Sent]"
-
-// The following should NOT be changed unless it seems
-// just AWFULLY necessary
-
-#define HUSTR_PLRGREEN	"Green: "
-#define HUSTR_PLRINDIGO	"Indigo: "
-#define HUSTR_PLRBROWN	"Brown: "
-#define HUSTR_PLRRED		"Red: "
-
-#define HUSTR_KEYGREEN	'g'
-#define HUSTR_KEYINDIGO	'i'
-#define HUSTR_KEYBROWN	'b'
-#define HUSTR_KEYRED		'r'
-
-//---------------------------------------------------------------------------
-//
 // AM_map.c
 //
 //---------------------------------------------------------------------------
@@ -250,26 +146,6 @@
 
 #define AMSTR_MARKEDSPOT	"Marked Spot"
 #define AMSTR_MARKSCLEARED	"All Marks Cleared"
-
-//---------------------------------------------------------------------------
-//
-// ST_stuff.c
-//
-//---------------------------------------------------------------------------
-
-#define STSTR_DQDON			"Degreelessness Mode On"
-#define STSTR_DQDOFF		"Degreelessness Mode Off"
-
-#define STSTR_KFAADDED		"Very Happy Ammo Added"
-
-#define STSTR_NCON			"No Clipping Mode ON"
-#define STSTR_NCOFF			"No Clipping Mode OFF"
-
-#define STSTR_BEHOLD		"inVuln, Str, Inviso, Rad, Allmap, or Lite-amp"
-#define STSTR_BEHOLDX		"Power-up Toggled"
-
-#define STSTR_CHOPPERS		"... doesn't suck - GM"
-#define STSTR_CLEV			"Changing Level..."
 
 //---------------------------------------------------------------------------
 //
@@ -374,56 +250,3 @@
 					"surrender without a fight. eyes\n"\
 					"wide, you go to meet your fate."
 
-/*
-#define E1TEXT	"Once you beat the big badasses and\n"\
-				"clean out the moon base you're supposed\n"\
-				"to win, aren't you? Aren't you? Where's\n"\
-				"your fat reward and ticket home? What\n"\
-				"the hell is this? It's not supposed to\n"\
-				"end this way!\n"\
-				"\n" \
-				"It stinks like rotten meat, but looks\n"\
-				"like the lost Deimos base.  Looks like\n"\
-				"you're stuck on The Shores of Hell.\n"\
-				"The only way out is through.\n"\
-				"\n"\
-				"To continue the DOOM experience, play\n"\
-				"The Shores of Hell and its amazing\n"\
-				"sequel, Inferno!\n"
-
-#define E2TEXT	"You've done it! The hideous cyber-\n"\
-				"demon lord that ruled the lost Deimos\n"\
-				"moon base has been slain and you\n"\
-				"are triumphant! But ... where are\n"\
-				"you? You clamber to the edge of the\n"\
-				"moon and look down to see the awful\n"\
-				"truth.\n" \
-				"\n"\
-				"Deimos floats above Hell itself!\n"\
-				"You've never heard of anyone escaping\n"\
-				"from Hell, but you'll make the bastards\n"\
-				"sorry they ever heard of you! Quickly,\n"\
-				"you rappel down to  the surface of\n"\
-				"Hell.\n"\
-				"\n" \
-				"Now, it's on to the final chapter of\n"\
-				"DOOM! -- Inferno."
-
-#define E3TEXT	"The loathsome spiderdemon that\n"\
-				"masterminded the invasion of the moon\n"\
-				"bases and caused so much death has had\n"\
-				"its ass kicked for all time.\n"\
-				"\n"\
-				"A hidden doorway opens and you enter.\n"\
-				"You've proven too tough for Hell to\n"\
-				"contain, and now Hell at last plays\n"\
-				"fair -- for you emerge from the door\n"\
-				"to see the green fields of Earth!\n"\
-				"Home at last.\n" \
-				"\n"\
-				"You wonder what's been happening on\n"\
-				"Earth while you were battling evil\n"\
-				"unleashed. It's good that no Hell-\n"\
-				"spawn could have come through that\n"\
-				"door with you ..."
-*/
