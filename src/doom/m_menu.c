42e190d0e2455f3d552028e60ddfc3f89a7e3260
Fabian Greffrath <fabian@greffrath.com>
2014-04-07 19:27:55 +0200

    Allow to set mouse sensitivity up to 20 in-game
    and stop crashing if values exceed thermometer range.
    
    closes https://github.com/fabiangreffrath/crispy-doom/issues/3

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -1065,7 +1065,7 @@ void M_DrawOptions(void)
                                       PU_CACHE));
 
     M_DrawThermo(OptionsDef.x, OptionsDef.y + LINEHEIGHT * (mousesens + 1),
-		 10, mouseSensitivity);
+		 20, mouseSensitivity);
 
     M_DrawThermo(OptionsDef.x,OptionsDef.y+LINEHEIGHT*(scrnsize+1),
 		 10,screenSize);
@@ -1252,7 +1252,7 @@ void M_ChangeSensitivity(int choice)
 	    mouseSensitivity--;
 	break;
       case 1:
-	if (mouseSensitivity < 9)
+	if (mouseSensitivity < 19)
 	    mouseSensitivity++;
 	break;
     }
@@ -1326,6 +1326,10 @@ M_DrawThermo
 	xx += 8;
     }
     V_DrawPatchDirect(xx, y, W_CacheLumpName(DEH_String("M_THERMR"), PU_CACHE));
+
+    // [crispy] do not crash anymore if value exceeds thermometer range
+    if (thermDot > thermWidth)
+        thermDot = thermWidth;
 
     V_DrawPatchDirect((x + 8) + thermDot * 8, y,
 		      W_CacheLumpName(DEH_String("M_THERMO"), PU_CACHE));
