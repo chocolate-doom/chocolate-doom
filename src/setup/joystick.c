10b94b8e2dfc86ccf3363a8a87e4a57a7fb24655
Simon Howard <fraggle@gmail.com>
2014-05-01 01:55:21 -0400

    setup: Enable joystick after calibration.
    If the user has just successfully calibrated the joystick, then
    turn on use_joystick as a convenience feature.

--- src/setup/joystick.c
+++ src/setup/joystick.c
@@ -505,6 +505,7 @@ static int CalibrationEventCallback(SDL_Event *event, void *user_data)
             // Finished?
             if (calibrate_stage == CALIBRATE_CENTER)
             {
+                use_joystick = 1;
                 TXT_CloseWindow(calibration_window);
             }
 
