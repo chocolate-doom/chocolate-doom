c669b89b0b962fb6ed9477a712f1f25e44aef090
Fabian Greffrath <fabian@greffrath.com>
2014-07-11 11:09:33 +0200

    Increase MAXVISSPRITES, MAXVISPLANES and MAXDRAWSEGS by factor 4
    fixes #14

--- src/doom/r_plane.c
+++ src/doom/r_plane.c
@@ -42,7 +42,7 @@ planefunction_t		ceilingfunc;
 //
 
 // Here comes the obnoxious "visplane".
-#define MAXVISPLANES	128*8
+#define MAXVISPLANES	128*8*4
 visplane_t		visplanes[MAXVISPLANES];
 visplane_t*		lastvisplane;
 visplane_t*		floorplane;
