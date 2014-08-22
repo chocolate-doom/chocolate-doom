e75940e324f18963e0394de8da10f2fd7d793ae7
Fabian Greffrath <fabian@greffrath.com>
2014-08-22 08:26:51 +0200

    fix crashes with IDMUS
    specifically prevent crash with IDMUS00 in Doom II and
    with IDMUS0x or IDMUSx0 in Doom
    
    fixes #34
    
    also, fix/add comments and declare "extern boolean P_CheckAmmo()"
    where it is actually needed
    
    # Conflicts:
    #	src/doom/st_stuff.c

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -555,7 +555,8 @@ ST_Responder (event_t* ev)
 	{
 	  musnum = mus_runnin + (buf[0]-'0')*10 + buf[1]-'0' - 1;
 	  
-	  if (((buf[0]-'0')*10 + buf[1]-'0') > 35
+	  // [crispy] prevent crash with IDMUS00
+	  if ((((buf[0]-'0')*10 + buf[1]-'0') > 35 || musnum < mus_runnin)
        && gameversion >= exe_doom_1_8)
 	    plyr->message = DEH_String(STSTR_NOMUS);
 	  else
@@ -565,7 +566,8 @@ ST_Responder (event_t* ev)
 	{
 	  musnum = mus_e1m1 + (buf[0]-'1')*9 + (buf[1]-'1');
 	  
-	  if (((buf[0]-'1')*9 + buf[1]-'1') > 31)
+	  // [crispy] prevent crash with IDMUS0x or IDMUSx0
+	  if (((buf[0]-'1')*9 + buf[1]-'1') > 31 || buf[0] < '1' || buf[1] < '1')
 	    plyr->message = DEH_String(STSTR_NOMUS);
 	  else
 	    S_ChangeMusic(musnum, 1);
