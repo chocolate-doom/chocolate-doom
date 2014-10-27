23a4026d4413716709b5e22dbf98a2ec57061088
Fabian Greffrath <fabian@greffrath.com>
2014-10-27 11:12:41 +0100

    fix gray-to-color hack

--- src/v_trans.c
+++ src/v_trans.c
@@ -186,15 +186,7 @@ byte V_Colorize (byte *playpal, int cr, byte source)
 	// [crispy] hack some colors into gray shades
 	if (hsv.y < CTOLERANCE)
 	{
-	    if (hsv.z < 0.5)
-	    {
-		hsv.y = 1.0;
-	    }
-	    else
-	    {
-		hsv.y = 1.0 - hsv.z;
-		hsv.z = 1.0;
-	    }
+	    hsv.y = 1.0;
 	}
 
 	if (cr == CR_GREEN)
