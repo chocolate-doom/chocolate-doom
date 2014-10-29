8b7c9f633f8ef93e09a4ce3fe909868343e706ff
fx02 <zbuzanic@gmail.com>
2014-10-29 08:34:59 +0100

    Typo error

--- src/doom/d_main.c
+++ src/doom/d_main.c
@@ -390,7 +390,6 @@ void D_BindVariables(void)
     I_BindVideoVariables();
     I_BindJoystickVariables();
     I_BindSoundVariables();
- ff   
     M_BindBaseControls();
     M_BindWeaponControls();
     M_BindMapControls();
