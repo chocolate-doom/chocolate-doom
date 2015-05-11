83786026b302c64313180510119389f401782310
Fabian Greffrath <fabian@greffrath.com>
2015-05-11 18:22:11 +0200

    improve dead player respawning angle

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -476,7 +476,7 @@ ST_Responder (event_t* ev)
 
 	    mt.x = plyr->mo->x >> FRACBITS;
 	    mt.y = plyr->mo->y >> FRACBITS;
-	    mt.angle = plyr->mo->angle*(uint64_t)45/ANG45;
+	    mt.angle = (plyr->mo->angle + ANG45/2)*(uint64_t)45/ANG45;
 	    mt.type = consoleplayer + 1;
 
 	    P_SpawnPlayer(&mt);
