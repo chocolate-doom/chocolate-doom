7d8a9f3f2ecea118993fc5c828bdf4f0e4c2f154
Fabian Greffrath <fabian@greffrath.com>
2015-05-27 19:35:05 +0200

    desktop: Add some additional keywords to *.desktop files
    fixes #511

--- src/doom/r_plane.c
+++ src/doom/r_plane.c
@@ -42,7 +42,7 @@ planefunction_t		ceilingfunc;
 //
 
 // Here comes the obnoxious "visplane".
-#define MAXVISPLANES	128
+#define MAXVISPLANES	128*8
 visplane_t		visplanes[MAXVISPLANES];
 visplane_t*		lastvisplane;
 visplane_t*		floorplane;
