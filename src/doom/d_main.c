a121f771e56356a5b9151874a2f2bc1cc82cec6c
Fabian Greffrath <fabian@greffrath.com>
2015-06-12 10:02:51 +0200

    only interpret second argument to -warp parameter as startmap, if it is not another parameter (i.e. begins with '-')

--- src/doom/d_main.c
+++ src/doom/d_main.c
@@ -2018,7 +2018,8 @@ void D_DoomMain (void)
         {
             startepisode = myargv[p+1][0]-'0';
 
-            if (p + 2 < myargc)
+            // [crispy] only if second argument is not another option
+            if (p + 2 < myargc && myargv[p+2][0] != '-')
             {
                 startmap = myargv[p+2][0]-'0';
             }
