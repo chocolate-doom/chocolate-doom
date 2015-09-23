bc2141f5c83c842d22bca39c496a48617993ddb1
Fabian Greffrath <fabian@greffrath.com>
2015-09-23 13:23:44 +0200

    use overflow-safe R_PointToAngle() only during rendering
    # Conflicts:
    #	src/doom/r_bsp.c

--- src/doom/r_bsp.c
+++ src/doom/r_bsp.c
@@ -423,8 +423,8 @@ boolean R_CheckBBox (fixed_t*	bspcoord)
     y2 = bspcoord[checkcoord[boxpos][3]];
     
     // check clip list for an open space
-    angle1 = R_PointToAngle (x1, y1) - viewangle;
-    angle2 = R_PointToAngle (x2, y2) - viewangle;
+    angle1 = R_PointToAngleCrispy (x1, y1) - viewangle;
+    angle2 = R_PointToAngleCrispy (x2, y2) - viewangle;
 	
     span = angle1 - angle2;
 
