8afaf4ed3702d7e0508aa61c71060c6761a6fc1b
Fabian Greffrath <fabian@greffrath.com>
2015-10-05 09:45:36 +0200

    fix valgrind warnings due to uninitialized use of postcount

--- src/doom/r_data.c
+++ src/doom/r_data.c
@@ -409,6 +409,7 @@ void R_GenerateLookup (int texnum)
     patchcount = (byte *) Z_Malloc(texture->width, PU_STATIC, &patchcount);
     postcount = (byte *) Z_Malloc(texture->width, PU_STATIC, &postcount);
     memset (patchcount, 0, texture->width);
+    memset (postcount, 0, texture->width);
     patch = texture->patches;
 
     for (i=0 , patch = texture->patches;
