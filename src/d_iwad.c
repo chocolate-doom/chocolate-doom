0cd3b7e5f8634468676b3b6ff1a525837bf3de2d
Simon Howard <fraggle@soulsphere.org>
2016-06-05 11:45:26 -0400

    setup: Fix crash with Chex Quest warp select.
    The level selection dialog was crashing on open when Chex Quest was
    selected as an IWAD. This appears to have been caused by b0585fa6
    which changed Chex Quest to be considered as a retail IWAD rather
    than a shareware one, but did not update the tables in d_iwad.c to
    match. Change the tables there to match and fix the crash.

--- src/d_iwad.c
+++ src/d_iwad.c
@@ -38,7 +38,7 @@ static const iwad_t iwads[] =
     { "tnt.wad",      pack_tnt,  commercial, "Final Doom: TNT: Evilution" },
     { "doom.wad",     doom,      retail,     "Doom" },
     { "doom1.wad",    doom,      shareware,  "Doom Shareware" },
-    { "chex.wad",     pack_chex, shareware,  "Chex Quest" },
+    { "chex.wad",     pack_chex, retail,     "Chex Quest" },
     { "hacx.wad",     pack_hacx, commercial, "Hacx" },
     { "freedm.wad",   doom2,     commercial, "FreeDM" },
     { "freedoom2.wad", doom2,    commercial, "Freedoom: Phase 2" },
