7c846a6fe2e472ff6070e185704ce20e6510bcf8
Fabian Greffrath <fabian@greffrath.com>
2016-06-18 12:34:10 +0200

    repair rendering of the "loading" disk icon
    Fixes #117

--- src/v_diskicon.c
+++ src/v_diskicon.c
@@ -76,7 +76,7 @@ static void SaveDiskData(char *disk_lump, int xoffs, int yoffs)
 
     // Draw the patch and save the result to disk_data.
     disk = W_CacheLumpName(disk_lump, PU_STATIC);
-    V_DrawPatch(loading_disk_xoffs, loading_disk_yoffs, disk);
+    V_DrawPatch(loading_disk_xoffs >> hires, loading_disk_yoffs >> hires, disk);
     CopyRegion(disk_data, LOADING_DISK_W,
                tmpscreen + yoffs * SCREENWIDTH + xoffs, SCREENWIDTH,
                LOADING_DISK_W, LOADING_DISK_H);
