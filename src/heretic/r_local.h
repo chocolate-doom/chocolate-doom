6dfcf352c2db09d15fa803c673a9fe06609dbee9
Mike DeRoitr <aynrandjuggalo@gmail.com>
2016-11-18 18:54:35 -0600

    Update MAXVISSPRITES and MAXDRAWSEGS limits

--- src/heretic/r_local.h
+++ src/heretic/r_local.h
@@ -184,7 +184,7 @@ typedef struct drawseg_s
 #define SIL_TOP		2
 #define	SIL_BOTH	3
 
-#define	MAXDRAWSEGS		256
+#define	MAXDRAWSEGS		256*8
 
 // A vissprite_t is a thing that will be drawn during a refresh
 typedef struct vissprite_s
@@ -400,7 +400,7 @@ void R_PrecacheLevel(void);
 //
 // R_things.c
 //
-#define	MAXVISSPRITES	128
+#define	MAXVISSPRITES	128*8
 
 extern vissprite_t vissprites[MAXVISSPRITES], *vissprite_p;
 extern vissprite_t vsprsortedhead;
