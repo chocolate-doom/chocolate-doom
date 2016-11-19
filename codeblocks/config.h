945bcd7ec099cded2c5e70972557e9339862f95c
Mike DeRoitr <aynrandjuggalo@gmail.com>
2016-11-18 20:11:48 -0600

    Update config.h

--- codeblocks/config.h
+++ codeblocks/config.h
@@ -1,30 +1,30 @@
 
 /* Name of package */
-#define PACKAGE "chocolate-doom"
+#define PACKAGE "lemon-doom"
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "fraggle@gmail.com"
+#define PACKAGE_BUGREPORT "aynrandjuggalo@gmail.com"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "Chocolate Doom"
+#define PACKAGE_NAME "Lemon Doom"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Chocolate Doom 2.2.1"
+#define PACKAGE_STRING "Lemon Doom 0.1"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "chocolate-doom"
+#define PACKAGE_TARNAME "lemon-doom"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.2.1"
+#define PACKAGE_VERSION "0.1"
 
 /* Change this when you create your awesome forked version */
-#define PROGRAM_PREFIX "chocolate-"
+#define PROGRAM_PREFIX "lemon-"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "2.2.1"
+#define VERSION "0.1"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
