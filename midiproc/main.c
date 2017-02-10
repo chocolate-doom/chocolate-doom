2f8ed99c11279ab932dbda6db0647f70c3280b7a
Alex Mayfield <alexmax2742@gmail.com>
2017-02-10 18:40:34 -0500

    Normalize header comments and file extensions

--- midiproc/main.c
+++ midiproc/main.c
@@ -1,36 +1,27 @@
-// Emacs style mode select   -*- C++ -*-
-//-----------------------------------------------------------------------------
 //
 // Copyright(C) 2012 James Haley
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-//-----------------------------------------------------------------------------
 //
 // DESCRIPTION:
 //
 // Win32/SDL_mixer MIDI RPC Server
 //
-// Uses RPC to communicate with Eternity. This allows this separate process to
+// Uses RPC to communicate with Doom. This allows this separate process to
 // have its own independent volume control even under Windows Vista and up's 
 // broken, stupid, completely useless mixer model that can't assign separate
 // volumes to different devices for the same process.
 //
 // Seriously, how did they screw up something so fundamental?
 //
-//-----------------------------------------------------------------------------
 
 #include <windows.h>
 #include <stdlib.h>
--- src/i_midirpc.h
+++ src/i_midirpc.h
@@ -1,31 +1,19 @@
-// Emacs style mode select   -*- C++ -*-
-//-----------------------------------------------------------------------------
 //
-// Copyright (C) 2013 James Haley et al.
+// Copyright(C) 2013 James Haley et al.
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/
-//
-// Additional terms and conditions compatible with the GPLv3 apply. See the
-// file COPYING-EE for details.
-//
-//----------------------------------------------------------------------------
-//
 // DESCRIPTION:
+//     Client Interface to RPC Midi Server
 //
-// Client Interface to RPC Midi Server
-//
-//-----------------------------------------------------------------------------
 
 #ifndef I_MIDIRPC_H__
 #define I_MIDIRPC_H__
