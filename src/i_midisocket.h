73eac9f95f513260a106f10cf9f8501ccc7eb36c
Alex Mayfield <alexmax2742@gmail.com>
2017-02-10 23:20:56 -0500

    Add my name to midisocket file headers

--- src/i_midisocket.h
+++ src/i_midisocket.h
@@ -1,5 +1,6 @@
 //
 // Copyright(C) 2013 James Haley et al.
+// Copyright(C) 2017 Alex Mayfield
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
