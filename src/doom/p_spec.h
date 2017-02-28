31c6cbb9e82e9c3cb03c99d91f1246485ca28457
Fabian Greffrath <fabian@greffrath.com>
2017-02-28 20:02:20 +0100

    double MAXBUTTONS value
    to allow for SPECHITS on E2M5

--- src/doom/p_spec.h
+++ src/doom/p_spec.h
@@ -238,7 +238,7 @@ typedef struct
 #define MAXSWITCHES		50
 
  // 4 players, 4 buttons each at once, max.
-#define MAXBUTTONS		16
+#define MAXBUTTONS		16*2
 
  // 1 second, in ticks. 
 #define BUTTONTIME      35             
