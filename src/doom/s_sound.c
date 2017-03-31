26269b6703cfab0aa167f18f48df31460ff3809a
Fabian Greffrath <fabian@greffrath.com>
2017-03-31 15:14:29 +0200

    support IDMUS4x for dedicated music tracks for the 4th episode
    Achieve this by
    
    1) initializing the music->lumpnum fields for the 9 music tracks in
       question (-1 means the track is not available, 0 means not checked
       yet)
    
    2) generalizing the crash prevention checks in the IDMUS cheat to
       merely make sure the resulting music track is supposed to be
       available and adding a further check for the music->lumpnum field
    
    The actual code in S_Start() could be simplified a lot due to this.

--- src/doom/s_sound.c
+++ src/doom/s_sound.c
@@ -175,6 +175,16 @@ void S_Init(int sfxVolume, int musicVolume)
     }
 
     I_AtExit(S_Shutdown, true);
+
+    // [crispy] initialize dedicated music tracks for the 4th episode
+    for (i = mus_e4m1; i <= mus_e4m9; i++)
+    {
+        musicinfo_t *const music = &S_music[i];
+        char namebuf[9];
+
+        M_snprintf(namebuf, sizeof(namebuf), "d_%s", DEH_String(music->name));
+        music->lumpnum = W_CheckNumForName(namebuf);
+    }
 }
 
 void S_Shutdown(void)
@@ -292,21 +302,9 @@ void S_Start(void)
 
             // [crispy] support dedicated music tracks for the 4th episode
             {
-                int sp_mnum;
-                musicinfo_t *music;
+                const int sp_mnum = mus_e1m1 + 3 * 9 + gamemap - 1;
 
-                sp_mnum = mus_e1m1 + 3*9 + gamemap-1;
-                music = &S_music[sp_mnum];
-
-                if (!music->lumpnum)
-                {
-                    char namebuf[9];
-
-                    M_snprintf(namebuf, sizeof(namebuf), "d_%s", DEH_String(music->name));
-                    music->lumpnum = W_CheckNumForName(namebuf);
-                }
-
-                if (music->lumpnum > 0)
+                if (S_music[sp_mnum].lumpnum > 0)
                 {
                     mnum = sp_mnum;
                 }
