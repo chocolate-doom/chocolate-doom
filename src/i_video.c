c47a715ed8f0fb958a9ece5750f75856f252caa6
Fabian Greffrath <fabian@greffrath.com>
2017-05-31 08:14:09 +0200

    video: turn 500 ms delay into a constant

--- src/i_video.c
+++ src/i_video.c
@@ -189,6 +189,7 @@ static boolean window_focused = true;
 
 static boolean need_resize = false;
 static unsigned int last_resize_time;
+#define RESIZE_DELAY 500
 
 // Gamma correction level to use
 
@@ -711,7 +712,7 @@ void I_FinishUpdate (void)
 
     if (need_resize)
     {
-        if (SDL_GetTicks() > last_resize_time + 500)
+        if (SDL_GetTicks() > last_resize_time + RESIZE_DELAY)
         {
             int flags;
             // When the window is resized (we're not in fullscreen mode),
