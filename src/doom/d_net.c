11035e797c56d66d3afebdc5041a7fce26aba36d
Fabian Greffrath <fabian@greffrath.com>
2017-09-15 21:29:29 +0200

    add a -shorttics command line option
    I was really surprised today when ZeroMaster010 told me that he always
    has to start Doom with the -record parameter to practice for his
    record runs, because there is no other way to enable low-resolution
    turning during normal play.
    
    Why is that? I mean, we have a command line switch to enable
    full-resolution turning even when recording a demo, so why shouldn't
    there be a switch to force low-esolution turning even when not
    recording a demo. PrBoom+ already has this switch.

--- src/doom/d_net.c
+++ src/doom/d_net.c
@@ -152,8 +152,9 @@ static void SaveGameSettings(net_gamesettings_t *settings)
     settings->respawn_monsters = respawnparm;
     settings->timelimit = timelimit;
 
-    settings->lowres_turn = M_CheckParm("-record") > 0
-                         && M_CheckParm("-longtics") == 0;
+    settings->lowres_turn = (M_CheckParm("-record") > 0
+                          && M_CheckParm("-longtics") == 0)
+                          || M_CheckParm("-shorttics") > 0;
 }
 
 static void InitConnectData(net_connect_data_t *connect_data)
@@ -196,8 +197,9 @@ static void InitConnectData(net_connect_data_t *connect_data)
 
     // Are we recording a demo? Possibly set lowres turn mode
 
-    connect_data->lowres_turn = M_CheckParm("-record") > 0
-                             && M_CheckParm("-longtics") == 0;
+    connect_data->lowres_turn = (M_CheckParm("-record") > 0
+                              && M_CheckParm("-longtics") == 0)
+                              || M_CheckParm("-shorttics") > 0;
 
     // Read checksums of our WAD directory and dehacked information
 
