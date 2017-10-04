808d95c7990e90191bf0b1d33154a8fbb7f52b8d
Fabian Greffrath <fabian@greffrath.com>
2017-10-04 21:06:53 +0200

    git checkout upstream/master -- codeblocks

--- codeblocks/config.h
+++ codeblocks/config.h
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-
-/* Name of package */
-#define PACKAGE "crispy-doom"
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "fabian@greffrath.com"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "Crispy Doom"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Crispy Doom 4.1"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "crispy-doom"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "4.1"
-
-/* Change this when you create your awesome forked version */
-#define PROGRAM_PREFIX "crispy-"
-
-/* Define to 1 if you have the ANSI C header files. */
-#define STDC_HEADERS 1
-
-/* Version number of package */
-#define VERSION "4.1"
-
-/* Define to 1 if your processor stores words with the most significant byte
-   first (like Motorola and SPARC, unlike Intel and VAX). */
-/* #undef WORDS_BIGENDIAN */
-
-/* Declare netcode-compatibility with Chocolate Doom */
-#define NET_PACKAGE_STRING "Chocolate Doom 2.3.0"
-=======
 
 /* Name of package */
 #define PACKAGE "chocolate-doom"
@@ -45,13 +9,13 @@
 #define PACKAGE_NAME "Chocolate Doom"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Chocolate Doom 3.0.0-beta1"
+#define PACKAGE_STRING "Chocolate Doom 2.3.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "chocolate-doom"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.0.0-beta1"
+#define PACKAGE_VERSION "2.3.0"
 
 /* Change this when you create your awesome forked version */
 #define PROGRAM_PREFIX "chocolate-"
@@ -60,10 +24,9 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "3.0.0-beta1"
+#define VERSION "2.3.0"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
 /* #undef WORDS_BIGENDIAN */
 
->>>>>>> upstream/sdl2-branch
