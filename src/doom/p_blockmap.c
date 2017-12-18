4a2c87bc33aa6d4490d34b1611e6808d548dacc8
Fabian Greffrath <fabian@greffrath.com>
2017-12-18 08:46:57 +0100

    back to the GPL'ed P_CreateBlockMap implementation from MBF

--- src/doom/p_blockmap.c
+++ src/doom/p_blockmap.c
@@ -1,63 +1,7 @@
 //
 // Copyright(C) 1993-1996 Id Software, Inc.
-//
-// DESCRIPTION:
-//	[crispy] Create Blockmap
-//
-// This code is derived from id's "doombsp" binary space partitioner
-// which is accompanied with the following README.TXT:
-
-/*
-The source code for the binary space partitioner we used for DOOM is now
-available at ftp.uwp.edu: /incoming/id/doombsp.zip.
-
-The catch is that the source has a few objective-c constructs in it, so
-it will take some work to port to dos.  The only thing that will be a
-hassle is replacing the collection objects, the majority is just
-straight C.
-
-This code was written and extended, not evolved, so it probably isn't
-the cleanest thing in the world.  Please, PLEASE, PLEASE do not ask
-for support on this.  I have far too much occupying my time as it is.
-
-Our map editor does not work on wad files.  It saves an ascii text
-representation of the file, then launches doombsp to process that into
-a wad file. I have included the input and output for E1M1, so you can
-verify any porting work you perform.
-
-Having two programs allowed us to seperate the tasks well under
-NEXTSTEP, but people working on dos editors will probably want to
-integrate a version of the bsp code directly into the editor.
-
-If you are creating new DOOM maps for other people to use, we would
-apreciate it if the wadfiles you create use a PWAD identifier at the
-start of the file instead of the normal IWAD.  This causes DOOM to tell
-the user that they are playing a modified version, and no technical
-support will be given.
-
-If you are creating a map editor for distribution to other people,
-contact Jay WIlbur (jayw@idsoftware.com) about getting a license
-agreement for the use of our trademark, etc.  Its not a money issue,
-just some legal jazz.
-
-BTW, there IS a bug in here that can cause up to a four pixel wide
-column to be drawn out of order, causing a more distant floor and
-ceiling plane to stream farther forward than it should.  You can
-sometimes see this on E1M1 looking towards the imp up on the ledge
-at the entrance to the zig zag room.  A few pixel wide column of
-slime streams down to the right of the walkway.  It takes a bit of
-fidgeting with the mouse to find the spot.  If someone out there
-tracks it down, let me know...
-
-Have fun!
-
-John Carmack
-Id Software
-*/
-
-// At the time of the Crispy Doom 4.1 release the license agreement
-// is still pending. For all code modifications the following applies:
-//
+// Copyright(C) 1999 id Software, Chi Hoang, Lee Killough, Jim Flynn, Rand Phares, Ty Halderman
+// Copyright(C) 2005-2014 Simon Howard
 // Copyright(C) 2017 Fabian Greffrath
 //
 // This program is free software; you can redistribute it and/or
@@ -69,346 +13,194 @@ Id Software
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
+//
+// DESCRIPTION:
+//	[crispy] Create Blockmap
+//
 
 #include <stdlib.h>
 #include "i_system.h"
 #include "p_local.h"
 #include "z_zone.h"
 
-#define BLOCKMAPSIZE 0x10000
+// [crispy] taken from mbfsrc/P_SETUP.C:547-707, slightly adapted
 
-static int32_t *datalist, *data_p, *pointer_p;
-static size_t datalist_size;
-
-static fixed_t xl, xh, yl, yh;
-
-// [crispy] adapted from doombsp/BUILDBSP.M:23-76
-static int P_PointOnSide (vertex_t *p, line_t *l)
+void P_CreateBlockMap(void)
 {
-	int dx, dy, ldx, ldy;
-	int left, right;
-	int64_t a, b, c, d;
+  register int i;
+  fixed_t minx = INT_MAX, miny = INT_MAX, maxx = INT_MIN, maxy = INT_MIN;
 
-	// [crispy] dead code, only diagonal lines here
-	/*
-	if (!l->dx)
+  // First find limits of map
+
+  for (i=0; i<numvertexes; i++)
+    {
+      if (vertexes[i].x >> FRACBITS < minx)
+	minx = vertexes[i].x >> FRACBITS;
+      else
+	if (vertexes[i].x >> FRACBITS > maxx)
+	  maxx = vertexes[i].x >> FRACBITS;
+      if (vertexes[i].y >> FRACBITS < miny)
+	miny = vertexes[i].y >> FRACBITS;
+      else
+	if (vertexes[i].y >> FRACBITS > maxy)
+	  maxy = vertexes[i].y >> FRACBITS;
+    }
+
+  // [crispy] doombsp/DRAWING.M:175-178
+  minx -= 8; miny -= 8;
+  maxx += 8; maxy += 8;
+
+  // Save blockmap parameters
+
+  bmaporgx = minx << FRACBITS;
+  bmaporgy = miny << FRACBITS;
+  bmapwidth  = ((maxx-minx) >> MAPBTOFRAC) + 1;
+  bmapheight = ((maxy-miny) >> MAPBTOFRAC) + 1;
+
+  // Compute blockmap, which is stored as a 2d array of variable-sized lists.
+  //
+  // Pseudocode:
+  //
+  // For each linedef:
+  //
+  //   Map the starting and ending vertices to blocks.
+  //
+  //   Starting in the starting vertex's block, do:
+  //
+  //     Add linedef to current block's list, dynamically resizing it.
+  //
+  //     If current block is the same as the ending vertex's block, exit loop.
+  //
+  //     Move to an adjacent block by moving towards the ending block in
+  //     either the x or y direction, to the block which contains the linedef.
+
+  {
+    typedef struct { int n, nalloc, *list; } bmap_t;  // blocklist structure
+    unsigned tot = bmapwidth * bmapheight;            // size of blockmap
+    bmap_t *bmap = calloc(sizeof *bmap, tot);         // array of blocklists
+    int x, y, adx, ady, bend;
+
+    for (i=0; i < numlines; i++)
+      {
+	int dx, dy, diff, b;
+
+	// starting coordinates
+	if (crispy->fliplevels)
 	{
-		if (p->x > l->v1->x - 2 * FRACUNIT && p->x < l->v1->x + 2 * FRACUNIT)
-		{
-			return -1;
-		}
-
-		if (p->x < l->v1->x)
-		{
-			return l->dy > 0;
-		}
-
-		return l->dy < 0;
-	}
-
-	if (!l->dy)
-	{
-		if (p->y > l->v1->y - 2 * FRACUNIT && p->y < l->v1->y + 2 * FRACUNIT)
-		{
-			return -1;
-		}
-
-		if (p->y < l->v1->y)
-		{
-			return l->dx < 0;
-		}
-
-		return l->dx > 0;
-	}
-	*/
-
-	dx = (l->v1->x - p->x) >> FRACBITS;
-	dy = (l->v1->y - p->y) >> FRACBITS;
-	ldx = l->dx >> FRACBITS;
-	ldy = l->dy >> FRACBITS;
-
-	a = ldx * ldx + ldy * ldy;
-	b = 2 * (ldx * dx + ldy * dy);
-	c = dx * dx + dy * dy - 2 * 2; // 2 unit radius
-	d = b * b - 4 * a * c;
-
-	if (d > 0)
-	{
-		// within four pixels of line
-		return -1;
-	}
-
-	left = ldy * -dx;
-	right = -dy * ldx;
-
-	// allow slop
-	if (left == right)
-	{
-		// on line
-		return -1;
-	}
-
-	if (right < left)
-	{
-		// front side
-		return 0;
-	}
-
-	// back side
-	return 1;
-}
-
-// [crispy] adapted from doombsp/SAVEBLCK.M
-static boolean LineContact (line_t *wl)
-{
-	vertex_t *p1, *p2, pt1, pt2, pd;
-	fixed_t lxl, lxh, lyl, lyh;
-	line_t ld;
-	int s1, s2;
-
-	p1 = wl->v1;
-	p2 = wl->v2;
-
-	ld.v1 = &pd;
-	ld.v1->x = p1->x;
-	ld.v1->y = p1->y;
-	ld.dx = p2->x - p1->x;
-	ld.dy = p2->y - p1->y;
-
-	if (p1->x < p2->x)
-	{
-		lxl = p1->x;
-		lxh = p2->x;
+	    x = (lines[i].v2->x >> FRACBITS) - minx;
+	    y = (lines[i].v2->y >> FRACBITS) - miny;
 	}
 	else
 	{
-		lxl = p2->x;
-		lxh = p1->x;
+	    x = (lines[i].v1->x >> FRACBITS) - minx;
+	    y = (lines[i].v1->y >> FRACBITS) - miny;
 	}
 
-	if (p1->y < p2->y)
+	// x-y deltas
+	adx = lines[i].dx >> FRACBITS, dx = adx < 0 ? -1 : 1;
+	ady = lines[i].dy >> FRACBITS, dy = ady < 0 ? -1 : 1;
+
+	// difference in preferring to move across y (>0) instead of x (<0)
+	diff = !adx ? 1 : !ady ? -1 :
+	  (((x >> MAPBTOFRAC) << MAPBTOFRAC) +
+	   (dx > 0 ? MAPBLOCKUNITS-1 : 0) - x) * (ady = abs(ady)) * dx -
+	  (((y >> MAPBTOFRAC) << MAPBTOFRAC) +
+	   (dy > 0 ? MAPBLOCKUNITS-1 : 0) - y) * (adx = abs(adx)) * dy;
+
+	// starting block, and pointer to its blocklist structure
+	b = (y >> MAPBTOFRAC)*bmapwidth + (x >> MAPBTOFRAC);
+
+	// ending block
+	if (crispy->fliplevels)
 	{
-		lyl = p1->y;
-		lyh = p2->y;
+	    bend = (((lines[i].v1->y >> FRACBITS) - miny) >> MAPBTOFRAC) *
+	        bmapwidth + (((lines[i].v1->x >> FRACBITS) - minx) >> MAPBTOFRAC);
 	}
 	else
 	{
-		lyl = p2->y;
-		lyh = p1->y;
+	    bend = (((lines[i].v2->y >> FRACBITS) - miny) >> MAPBTOFRAC) *
+	        bmapwidth + (((lines[i].v2->x >> FRACBITS) - minx) >> MAPBTOFRAC);
 	}
 
-	// no bbox intersections
-	if (lxl >= xh || lxh < xl || lyl >= yh || lyh < yl)
-	{
-		return false;
-	}
+	// delta for pointer when moving across y
+	dy *= bmapwidth;
 
-	if (FixedDiv(ld.dy, ld.dx) > 0)
-	{
-		// positive slope
-		pt1.x = xl;
-		pt1.y = yh;
-		pt2.x = xh;
-		pt2.y = yl;
-	}
-	else
-	{
-		// negative slope
-		pt1.x = xh;
-		pt1.y = yh;
-		pt2.x = xl;
-		pt2.y = yl;
-	}
+	// deltas for diff inside the loop
+	adx <<= MAPBTOFRAC;
+	ady <<= MAPBTOFRAC;
 
-	s1 = P_PointOnSide(&pt1, &ld);
-	s2 = P_PointOnSide(&pt2, &ld);
+	// Now we simply iterate block-by-block until we reach the end block.
+	while ((unsigned) b < tot)    // failsafe -- should ALWAYS be true
+	  {
+	    // Increase size of allocated list if necessary
+	    if (bmap[b].n >= bmap[b].nalloc)
+	      bmap[b].list = I_Realloc(bmap[b].list,
+				     (bmap[b].nalloc = bmap[b].nalloc ?
+				      bmap[b].nalloc*2 : 8)*sizeof*bmap->list);
 
-	return s1 != s2;
-}
+	    // Add linedef to end of list
+	    bmap[b].list[bmap[b].n++] = i;
 
-// [crispy] remove BLOCKMAP limit
-static int AddLineToBlockList (int i)
-{
-	if (data_p - datalist == datalist_size)
-	{
-		const int32_t *const datalist_old = datalist;
+	    // If we have reached the last block, exit
+	    if (b == bend)
+	      break;
 
-		datalist = I_Realloc(datalist, (datalist_size = 2 * datalist_size) * sizeof(*datalist));
+	    // Move in either the x or y direction to the next block
+	    if (diff < 0)
+	      diff += ady, b += dx;
+	    else
+	      diff -= adx, b += dy;
+	  }
+      }
 
-		data_p = datalist + (data_p - datalist_old);
-		pointer_p = datalist + (pointer_p - datalist_old);
-	}
+    // Compute the total size of the blockmap.
+    //
+    // Compression of empty blocks is performed by reserving two offset words
+    // at tot and tot+1.
+    //
+    // 4 words, unused if this routine is called, are reserved at the start.
 
-	*data_p++ = i;
+    {
+      int count = tot+6;  // we need at least 1 word per block, plus reserved's
 
-	return i > 0;
-}
+      for (i = 0; i < tot; i++)
+	if (bmap[i].n)
+	  count += bmap[i].n + 2; // 1 header word + 1 trailer word + blocklist
 
-static int GenerateBlockList (int x, int y)
-{
-	int i, count;
+      // Allocate blockmap lump with computed count
+      blockmaplump = Z_Malloc(sizeof(*blockmaplump) * count, PU_LEVEL, 0);
+    }
 
-	count = AddLineToBlockList(0);
+    // Now compress the blockmap.
+    {
+      int ndx = tot += 4;         // Advance index to start of linedef lists
+      bmap_t *bp = bmap;          // Start of uncompressed blockmap
 
-	xl = bmaporgx + x * MAPBLOCKSIZE;
-	xh = xl + MAPBLOCKSIZE;
-	yl = bmaporgy + y * MAPBLOCKSIZE;
-	yh = yl + MAPBLOCKSIZE;
+      blockmaplump[ndx++] = 0;    // Store an empty blockmap list at start
+      blockmaplump[ndx++] = -1;   // (Used for compression)
 
-	for (i = 0; i < numlines; i++)
-	{
-		line_t *const wl = &lines[i];
+      for (i = 4; i < tot; i++, bp++)
+	if (bp->n)                                      // Non-empty blocklist
+	  {
+	    blockmaplump[blockmaplump[i] = ndx++] = 0;  // Store index & header
+	    do
+	      blockmaplump[ndx++] = bp->list[--bp->n];  // Copy linedef list
+	    while (bp->n);
+	    blockmaplump[ndx++] = -1;                   // Store trailer
+	    free(bp->list);                             // Free linedef list
+	  }
+	else            // Empty blocklist: point to reserved empty blocklist
+	  blockmaplump[i] = tot;
 
-		// vertical
-		if (wl->v1->x == wl->v2->x)
-		{
-			if (wl->v1->x < xl || wl->v1->x >= xh)
-				continue;
+      free(bmap);    // Free uncompressed blockmap
+    }
+  }
 
-			if (wl->v1->y < wl->v2->y)
-			{
-				if (wl->v1->y >= yh || wl->v2->y < yl)
-					continue;
-			}
-			else
-			{
-				if (wl->v2->y >= yh || wl->v1->y < yl)
-					continue;
-			}
-
-			count += AddLineToBlockList(i);
-			continue;
-		}
-
-		// horizontal
-		if (wl->v1->y == wl->v2->y)
-		{
-			if (wl->v1->y < yl || wl->v1->y >= yh)
-				continue;
-
-			if (wl->v1->x < wl->v2->x)
-			{
-				if (wl->v1->x >= xh || wl->v2->x < xl)
-					continue;
-			}
-			else
-			{
-				if (wl->v2->x >= xh || wl->v1->x < xl)
-					continue;
-			}
-
-			count += AddLineToBlockList(i);
-			continue;
-		}
-
-		// diagonal
-		if (LineContact(wl))
-		{
-			count += AddLineToBlockList(i);
-		}
-	}
-
-	// end of list marker
-	count += AddLineToBlockList(-1);
-
-	return count;
-}
-
-// [crispy] adapted from doombsp/SAVEBLCK.M:SaveBlocks()
-void P_CreateBlockMap (void)
-{
-	int x ,y;
-	int numblocks, empty_block, compress;
-
-	{
-		int i;
-		int minx = INT_MAX, miny = INT_MAX, maxx = INT_MIN, maxy = INT_MIN;
-
-		for (i = 0; i < numvertexes; i++)
-		{
-			const vertex_t *const v = &vertexes[i];
-			const int vx = v->x >> FRACBITS, vy = v->y >> FRACBITS;
-
-			if (vx < minx)
-				minx = vx;
-			else
-			if (vx > maxx)
-				maxx = vx;
-
-			if (vy < miny)
-				miny = vy;
-			else
-			if (vy > maxy)
-				maxy = vy;
-		}
-
-		// [crispy] doombsp/DRAWING.M:175-178
-		minx -= 8; miny -= 8;
-		maxx += 8; maxy += 8;
-
-		bmaporgx = minx << FRACBITS;
-		bmaporgy = miny << FRACBITS;
-		bmapwidth = ((maxx - minx) + MAPBLOCKUNITS) / MAPBLOCKUNITS;
-		bmapheight = ((maxy - miny) + MAPBLOCKUNITS) / MAPBLOCKUNITS;
-	}
-
-	// [crispy] remove BLOCKMAP limit
-	numblocks = bmapwidth * bmapheight;
-	datalist_size = ((numblocks + 6) / BLOCKMAPSIZE + 1) * BLOCKMAPSIZE; // [crispy] 6 = 4 (header) + 2 (empty block)
-	datalist = I_Realloc(datalist, datalist_size * sizeof(*datalist));
-
-	// [crispy] pointer to the offsets
-	pointer_p = datalist;
-	*pointer_p++ = bmaporgx;
-	*pointer_p++ = bmaporgy;
-	*pointer_p++ = bmapwidth;
-	*pointer_p++ = bmapheight;
-
-	// [crispy] pointer to the blocklists
-	data_p = pointer_p + numblocks;
-
-	// [crispy] detect empty blocks to compress BLOCKMAP
-	empty_block = data_p - datalist;
-	*data_p++ = 0;
-	*data_p++ = -1;
-	compress = 0;
-
-	for (y = 0; y < bmapheight; y++)
-	{
-		for (x = 0; x < bmapwidth; x++)
-		{
-			*pointer_p++ = data_p - datalist;
-
-			// [crispy] detect empty blocks to compress BLOCKMAP
-			if (!GenerateBlockList(x, y))
-			{
-				// [crispy] one step back, point offset to empty block, restore
-				pointer_p--; *pointer_p++ = empty_block;
-
-				// [crispy] two steps back: 0 and -1
-				data_p--; data_p--;
-
-				compress++;
-			}
-		}
-	}
-
-	{
-		const long blockmapsize = data_p - datalist;
-
-		fprintf(stderr, "P_CreateBlockMap: (%d, %d) = %ld (%3.1f%%)\n",
-			bmapwidth, bmapheight,
-			blockmapsize * sizeof(*blockmaplump),
-			100.f * blockmapsize / (blockmapsize + 2 * compress));
-
-		blockmaplump = Z_Malloc(blockmapsize * sizeof(*blockmaplump), PU_LEVEL, 0);
-		memcpy(blockmaplump, datalist, blockmapsize * sizeof(*blockmaplump));
-		free(datalist); datalist = NULL;
-	}
-
-	// [crispy] copied over from P_LoadBlockMap()
-	blocklinks = Z_Malloc(numblocks * sizeof(*blocklinks), PU_LEVEL, 0);
-	memset(blocklinks, 0, numblocks * sizeof(*blocklinks));
-
-	blockmap = blockmaplump + 4;
+  // [crispy] copied over from P_LoadBlockMap()
+  {
+    int count = sizeof(*blocklinks) * bmapwidth * bmapheight;
+    blocklinks = Z_Malloc(count, PU_LEVEL, 0);
+    memset(blocklinks, 0, count);
+    blockmap = blockmaplump+4;
+  }
 }
