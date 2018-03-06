27a28b8ad9a594fd395ba9c7437752098cdee75d
Turo Lamminen <turotl@gmail.com>
2018-03-06 19:14:45 +0200

    Make TXT_NewCheckBox label parameter const

--- textscreen/txt_checkbox.h
+++ textscreen/txt_checkbox.h
@@ -55,7 +55,7 @@ struct txt_checkbox_s
  * @return              Pointer to the new checkbox.
  */
 
-txt_checkbox_t *TXT_NewCheckBox(char *label, int *variable);
+txt_checkbox_t *TXT_NewCheckBox(const char *label, int *variable);
 
 /**
  * Create a new inverted checkbox.
