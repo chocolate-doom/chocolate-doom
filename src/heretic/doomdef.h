fe5173fde94c26c498266ad89b21a985aea7900f
Turo Lamminen <turotl@gmail.com>
2018-04-24 20:13:06 +0300

    heretic: Make MN_DrTextB text parameter const

--- src/heretic/doomdef.h
+++ src/heretic/doomdef.h
@@ -820,7 +820,7 @@ void MN_Ticker(void);
 void MN_Drawer(void);
 void MN_DrTextA(const char *text, int x, int y);
 int MN_TextAWidth(const char *text);
-void MN_DrTextB(char *text, int x, int y);
+void MN_DrTextB(const char *text, int x, int y);
 int MN_TextBWidth(char *text);
 
 #include "sounds.h"
