06bec5ae5a50c5c746401d4ba61890da17fee80a
Turo Lamminen <turotl@gmail.com>
2018-08-07 17:58:21 +0300

    hexen: Make R_FlatNumForName parameter const

--- src/hexen/h2def.h
+++ src/hexen/h2def.h
@@ -885,7 +885,7 @@ void R_DrawTopBorder(void);
 void R_SetViewSize(int blocks, int detail);
 // called by M_Responder
 
-int R_FlatNumForName(char *name);
+int R_FlatNumForName(const char *name);
 
 int R_TextureNumForName(char *name);
 int R_CheckTextureNumForName(char *name);
