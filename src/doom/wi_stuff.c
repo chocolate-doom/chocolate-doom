1ca98e292f83aea74d48967a3234110fd70e9912
Fabian Greffrath <fabian@greffrath.com>
2018-10-18 23:17:25 +0200

    fix some patch drawing on intermission screens

--- src/doom/wi_stuff.c
+++ src/doom/wi_stuff.c
@@ -421,7 +421,7 @@ void WI_drawLF(void)
     if ((gamemode == commercial && (unsigned)wbs->last >= NUMCMAPS) ||
         (gamemode != commercial && (unsigned)wbs->last >= (crispy->havee1m10 ? NUMMAPS + 1 : NUMMAPS)))
     {
-        V_DrawPatch((SCREENWIDTH - SHORT(finished->width)) / 2, y, finished);
+        V_DrawPatch((ORIGWIDTH - SHORT(finished->width)) / 2, y, finished);
         return;
     }
 
@@ -439,7 +439,7 @@ void WI_drawLF(void)
     else if (wbs->last == NUMCMAPS)
     {
         // MAP33 - draw "Finished!" only
-        V_DrawPatch((SCREENWIDTH - SHORT(finished->width)) / 2, y, finished);
+        V_DrawPatch((ORIGWIDTH - SHORT(finished->width)) / 2, y, finished);
     }
     else if (wbs->last > NUMCMAPS)
     {
