8f41fda4c4db660938f3cdb68ed7d0beaca4c8e5
Simon Howard <fraggle@soulsphere.org>
2018-11-18 23:17:26 -0500

    net: Initial reimplementation of vanilla protocol.
    This is mostly complete and seems to work for the most part. The
    drone peer functionality has not yet been tested.

---
+++ src/net_vanilla.h
@@ -0,0 +1,39 @@
+//
+// Copyright(C) 2018 Simon Howard
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+
+#ifndef NET_VANILLA_H
+#define NET_VANILLA_H
+
+#include "net_defs.h"
+
+typedef struct
+{
+    unsigned int version;
+
+    net_addr_t *addrs[MAXNETNODES];
+    unsigned int num_nodes;
+
+    unsigned int consoleplayer;
+    unsigned int num_players;
+
+    unsigned int extratics;
+} net_vanilla_settings_t;
+
+void NET_VanillaInit(net_context_t *context, net_vanilla_settings_t *settings);
+boolean NET_VanillaSyncSettings(net_gamesettings_t *settings);
+void NET_VanillaSendTiccmd(ticcmd_t *ticcmd, int maketic);
+void NET_VanillaQuit(void);
+void NET_VanillaRun(void);
+
+#endif /* #ifndef NET_CLIENT_H */
