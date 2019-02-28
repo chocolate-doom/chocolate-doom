f96076090f1cdfd7e64088cf2c6832f85fa1f487
Fabian Greffrath <fabian@greffrath.com>
2019-02-28 10:32:21 +0100

    only draw revealed secret sector boundaries in gold if "Show Revealed Secrets" is enabled
    Fixes #389, thanks @Zodomaniac

--- src/doom/am_map.c
+++ src/doom/am_map.c
@@ -1357,7 +1357,7 @@ void AM_drawWalls(void)
 		    cheating && (lines[i].frontsector->special == 9))
 		    AM_drawMline(&l, SECRETWALLCOLORS);
 #if defined CRISPY_HIGHLIGHT_REVEALED_SECRETS
-		// [crispy] draw revealed secrets in gold
+		// [crispy] draw revealed secret sector boundaries in gold
 		else
 		if (crispy->extautomap &&
 		    crispy->secretmessage && (lines[i].frontsector->oldspecial == 9))
@@ -1385,7 +1385,7 @@ void AM_drawWalls(void)
 		}
 #if defined CRISPY_HIGHLIGHT_REVEALED_SECRETS
 		// [crispy] draw revealed secret sector boundaries in gold
-		else if (crispy->extautomap &&
+		else if (crispy->extautomap && crispy->secretmessage &&
 		    (lines[i].backsector->oldspecial == 9 ||
 		    lines[i].frontsector->oldspecial == 9))
 		{
