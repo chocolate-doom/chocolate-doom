e49f36d0df164087db663bb3541ba817709cf046
Fabian Greffrath <fabian@greffrath.com>
2019-03-18 13:17:04 +0100

    Crispy colors for Crispy Network GUI
    Thanks @Zodomaniac for nagging me about it

--- src/net_gui.c
+++ src/net_gui.c
@@ -400,6 +400,12 @@ void NET_WaitForLaunch(void)
         exit(-1);
     }
 
+    // [crispy] Crispy colors for Crispy Network GUI
+    TXT_SetColor(TXT_COLOR_BLUE, 0x04, 0x14, 0x40); // Romero's "funky blue" color
+    TXT_SetColor(TXT_COLOR_BRIGHT_GREEN, 249, 227, 0);  // 0xF9, 0xE3, 0x00
+    TXT_SetColor(TXT_COLOR_CYAN, 220, 153, 0);          // 0xDC, 0x99, 0x00
+    TXT_SetColor(TXT_COLOR_BRIGHT_CYAN, 76, 160, 223);  // 0x4C, 0xA0, 0xDF
+
     I_InitWindowIcon();
 
     ParseCommandLineArgs();
