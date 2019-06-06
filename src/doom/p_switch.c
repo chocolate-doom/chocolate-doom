58389b35c7d7dc0b8ffa5ea9d3b8891850f7d92e
Fabian Greffrath <fabian@greffrath.com>
2019-06-06 09:55:26 +0200

    fixing playing up to three sounds from lines with more than one switch texture
    This time, a more conservative approach. It should be possible to
    play the switch sound again and again in you hit the same switch
    rapidly. Fixes a bug reported by Looper in the forums.
    
    @JNechaevsky Do you still have the test map at hand which we used to
    check the initial fix. It should still work as before, but now it
    should be possible to "spam" the switch and it should play all sounds.

--- src/doom/p_switch.c
+++ src/doom/p_switch.c
@@ -251,6 +251,7 @@ P_ChangeSwitchTexture
     int     texBot;
     int     i;
     int     sound;
+    boolean playsound = false;
 	
     if (!useAgain)
 	line->special = 0;
@@ -269,7 +270,8 @@ P_ChangeSwitchTexture
     {
 	if (switchlist[i] == texTop)
 	{
-	    S_StartSoundOnce(crispy->soundfix ? &line->soundorg : buttonlist->soundorg,sound); // [crispy] corrected sound source
+//	    S_StartSound(buttonlist->soundorg,sound);
+	    playsound = true;
 	    sides[line->sidenum[0]].toptexture = switchlist[i^1];
 
 	    if (useAgain)
@@ -282,7 +284,8 @@ P_ChangeSwitchTexture
 	{
 	    if (switchlist[i] == texMid)
 	    {
-		S_StartSoundOnce(crispy->soundfix ? &line->soundorg : buttonlist->soundorg,sound); // [crispy] corrected sound source
+//		S_StartSound(buttonlist->soundorg,sound);
+		playsound = true;
 		sides[line->sidenum[0]].midtexture = switchlist[i^1];
 
 		if (useAgain)
@@ -295,7 +298,8 @@ P_ChangeSwitchTexture
 	    {
 		if (switchlist[i] == texBot)
 		{
-		    S_StartSoundOnce(crispy->soundfix ? &line->soundorg : buttonlist->soundorg,sound); // [crispy] corrected sound source
+//		    S_StartSound(buttonlist->soundorg,sound);
+		    playsound = true;
 		    sides[line->sidenum[0]].bottomtexture = switchlist[i^1];
 
 		    if (useAgain)
@@ -305,6 +309,12 @@ P_ChangeSwitchTexture
 		}
 	    }
 	}
+    }
+
+    // [crispy] corrected sound source
+    if (playsound)
+    {
+	S_StartSound(crispy->soundfix ? &line->soundorg : buttonlist->soundorg,sound);
     }
 }
 
