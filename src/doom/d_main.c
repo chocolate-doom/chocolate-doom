dc22bb700c95d7078fb8ba2c6e225c41ec17ab3e
Fabian Greffrath <fabian@greffrath.com>
2019-09-02 21:06:26 +0200

    doom: no doom-all auto-load for Hacx and Chex Quest

--- src/doom/d_main.c
+++ src/doom/d_main.c
@@ -1517,10 +1517,13 @@ void D_DoomMain (void)
 
         // common auto-loaded files for all Doom flavors
 
-        autoload_dir = M_GetAutoloadDir("doom-all");
-        DEH_AutoLoadPatches(autoload_dir);
-        W_AutoLoadWADs(autoload_dir);
-        free(autoload_dir);
+        if (gamemission < pack_chex)
+        {
+            autoload_dir = M_GetAutoloadDir("doom-all");
+            DEH_AutoLoadPatches(autoload_dir);
+            W_AutoLoadWADs(autoload_dir);
+            free(autoload_dir);
+        }
 
         // auto-loaded files per IWAD
 
