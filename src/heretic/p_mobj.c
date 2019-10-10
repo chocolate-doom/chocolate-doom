3ae866e9f6c8bc5863344c95cf480fc6a865f805
drfrag <drfrag666@hotmail.com>
2019-10-10 21:16:52 +0200

    - Tweak 1K Deaths.

--- src/heretic/p_mobj.c
+++ src/heretic/p_mobj.c
@@ -870,6 +870,11 @@ mobj_t *P_SpawnMobj(fixed_t x, fixed_t y, fixed_t z, mobjtype_t type)
     mobj->flags2 = info->flags2;
     mobj->damage = info->damage;
     mobj->health = info->spawnhealth;
+    if (gameskill == sk_extreme)
+    {
+        // Half monster health in extreme mode
+        mobj->health >>= 1;
+    }
     if (gameskill != sk_nightmare)
     {
         mobj->reactiontime = info->reactiontime;
