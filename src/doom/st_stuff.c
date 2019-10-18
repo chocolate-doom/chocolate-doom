0707c20cc36735676786467d939cfe07692ac1f3
drfrag <drfrag666@hotmail.com>
2019-10-19 00:39:05 +0200

    Revert "fix crashes with IDMUS"
    This reverts commit f9afe75be045c7afb29a8d74c5b7deafd2ef8ac6.
    
    Bad merge.

--- src/doom/st_stuff.c
+++ src/doom/st_stuff.c
@@ -555,8 +555,8 @@ ST_Responder (event_t* ev)
 	{
 	  musnum = mus_runnin + (buf[0]-'0')*10 + buf[1]-'0' - 1;
 	  
-	  // [crispy] prevent crash with IDMUS00
-	  if ((((buf[0]-'0')*10 + buf[1]-'0') > 35 && gameversion >= exe_doom_1_8) || musnum < mus_runnin)
+	  if (((buf[0]-'0')*10 + buf[1]-'0') > 35
+       && gameversion >= exe_doom_1_8)
 	    plyr->message = DEH_String(STSTR_NOMUS);
 	  else
 	    S_ChangeMusic(musnum, 1);
@@ -565,8 +565,7 @@ ST_Responder (event_t* ev)
 	{
 	  musnum = mus_e1m1 + (buf[0]-'1')*9 + (buf[1]-'1');
 	  
-	  // [crispy] prevent crash with IDMUS0x or IDMUSx0
-	  if (((buf[0]-'1')*9 + buf[1]-'1') > 31 || buf[0] < '1' || buf[1] < '1')
+	  if (((buf[0]-'1')*9 + buf[1]-'1') > 31)
 	    plyr->message = DEH_String(STSTR_NOMUS);
 	  else
 	    S_ChangeMusic(musnum, 1);
