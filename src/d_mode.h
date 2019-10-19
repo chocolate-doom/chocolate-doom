b47d8f9d556f026f120a04489e4bd042c62f1f6d
James Canete <SmileTheory@gmail.com>
2019-10-19 16:07:00 -0700

    Doom v1.1 demo support.

--- src/d_mode.h
+++ src/d_mode.h
@@ -57,7 +57,8 @@ typedef enum
 typedef enum
 {
     exe_doom_1_0,    // Doom 1.0: shareware
-    exe_doom_1_2,    // Doom 1.2: shareware and registered
+    exe_doom_1_1,    // Doom 1.1: shareware and registered
+    exe_doom_1_2,    // Doom 1.2: "
     exe_doom_1_666,  // Doom 1.666: for shareware, registered and commercial
     exe_doom_1_7,    // Doom 1.7/1.7a: "
     exe_doom_1_8,    // Doom 1.8: "
