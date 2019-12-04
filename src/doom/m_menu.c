c177ccad4e0bba50da1e6a19837eee9bc92d7f57
Fabian Greffrath <fabian@greffrath.com>
2019-12-04 11:17:19 +0100

    restart demos from the map they were started
    Thanks Looper for the input.

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -2102,7 +2102,8 @@ static int G_ReloadLevel(void)
 
   if (gamestate == GS_LEVEL)
   {
-    G_DeferedInitNew(gameskill, gameepisode, gamemap);
+    // [crispy] restart demos from the map they were started
+    G_DeferedInitNew(gameskill, gameepisode, demorecording ? startmap : gamemap);
     result = true;
   }
 
