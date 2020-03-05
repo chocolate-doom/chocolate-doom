56131ddb5d970a1cfe52db309a4cde66415e8b3e
Fabian Greffrath <fabian@greffrath.com>
2020-03-05 12:20:57 +0100

    Explicitly write "crispy-doom" to extended savegame header (#538)
    to preserve savegame header compatibility across forks that might have different names, e.g. "so-doom"

--- src/doom/p_extsaveg.c
+++ src/doom/p_extsaveg.c
@@ -441,7 +441,7 @@ typedef struct
 
 static const extsavegdata_t extsavegdata[] =
 {
-	{PACKAGE_TARNAME, P_WritePackageTarname, NULL, 0},
+	{"crispy-doom", P_WritePackageTarname, NULL, 0},
 	{"wadfilename", P_WriteWadFileName, P_ReadWadFileName, 0},
 	{"extrakills", P_WriteExtraKills, P_ReadExtraKills, 1},
 	{"totalleveltimes", P_WriteTotalLevelTimes, P_ReadTotalLevelTimes, 1},
