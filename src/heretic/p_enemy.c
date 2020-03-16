a76bb7101e21d84b9a2fefa82a6db9b54974fe01
Fabian Greffrath <fabian@greffrath.com>
2020-03-16 09:38:00 +0100

    heretic: entirely remove INTERCEPTS and SPECHIT limits
    Formerly, the INTERCEPTS limit was only capped, i.e. the first 128
    intercepts would be checked as normal and the 129th would return
    false. This would merely prevent the segmentation fault, but the
    limit was still established nevertheless.
    
    No overflow emulation takes place at all.

--- src/heretic/p_enemy.c
+++ src/heretic/p_enemy.c
@@ -243,7 +243,7 @@ fixed_t yspeed[8] =
     { 0, 47000, FRACUNIT, 47000, 0, -47000, -FRACUNIT, -47000 };
 
 #define	MAXSPECIALCROSS		8
-extern line_t *spechit[MAXSPECIALCROSS];
+extern line_t **spechit; // [crispy] remove SPECHIT limit
 extern int numspechit;
 
 boolean P_Move(mobj_t * actor)
