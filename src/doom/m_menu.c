2bb51ef0b2e13fd3b6625a6dc699bc47a6b084a3
Fabian Greffrath <fabian@greffrath.com>
2020-04-28 13:32:26 +0200

    fix the "go to next level" cheat key for the Master Levels

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -2097,29 +2097,24 @@ static int G_ReloadLevel(void)
 
 static int G_GotoNextLevel(void)
 {
-  static byte doom_next[5][9] = {
+  byte doom_next[5][9] = {
     {12, 13, 19, 15, 16, 17, 18, 21, 14},
     {22, 23, 24, 25, 29, 27, 28, 31, 26},
     {32, 33, 34, 35, 36, 39, 38, 41, 37},
     {42, 49, 44, 45, 46, 47, 48, 51, 43},
     {52, 53, 54, 55, 56, 59, 58, 11, 57},
   };
-  static byte doom2_next[33] = {
-    0, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+  byte doom2_next[33] = {
+     2,  3,  4,  5,  6,  7,  8,  9, 10, 11,
     12, 13, 14, 15, 31, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 1,
     32, 16, 3
   };
-  static byte nerve_next[9] = {
+  byte nerve_next[9] = {
     2, 3, 4, 9, 6, 7, 8, 1, 5
   };
 
   int changed = false;
-
-  // [crispy] process only once
-  if (!doom2_next[0])
-  {
-    doom2_next[0] = 2;
 
     if (gamemode == commercial)
     {
@@ -2134,6 +2129,7 @@ static int G_GotoNextLevel(void)
         doom2_next[30] = 16;
         doom2_next[20] = 1;
       }
+
       if (gamemission == pack_master)
       {
         doom2_next[1] = 3;
@@ -2158,7 +2154,6 @@ static int G_GotoNextLevel(void)
         doom_next[0][4] = 11;
       }
     }
-  }
 
   if (gamestate == GS_LEVEL)
   {
