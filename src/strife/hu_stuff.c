a3e027a20bc15350ccee1051755a5e0d918b9ef8
Turo Lamminen <turol@iki.fi>
2020-04-28 17:57:05 +0300

    strife: Refactor chat macro defaults for const correctness
    strife version of f523e59e1db0015b5e6e25da863c387f19841eea

--- src/strife/hu_stuff.c
+++ src/strife/hu_stuff.c
@@ -58,19 +58,7 @@
 #define HU_INPUTWIDTH   64
 #define HU_INPUTHEIGHT  1
 
-char *chat_macros[10] =
-{
-    HUSTR_CHATMACRO0,
-    HUSTR_CHATMACRO1,
-    HUSTR_CHATMACRO2,
-    HUSTR_CHATMACRO3,
-    HUSTR_CHATMACRO4,
-    HUSTR_CHATMACRO5,
-    HUSTR_CHATMACRO6,
-    HUSTR_CHATMACRO7,
-    HUSTR_CHATMACRO8,
-    HUSTR_CHATMACRO9
-};
+char *chat_macros[10];
 
 // villsa [STRIFE]
 char player_names[8][16] =
