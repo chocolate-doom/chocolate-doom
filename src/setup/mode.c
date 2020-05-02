c7de9d61090d234506d9145b14351b201b03dba6
Turo Lamminen <turol@iki.fi>
2020-05-02 15:53:33 +0300

    setup: Make GetMissionForName parameter const

--- src/setup/mode.c
+++ src/setup/mode.c
@@ -224,7 +224,7 @@ static void SetMission(mission_config_t *config)
     M_SetConfigFilenames(config->config_file, config->extra_config_file);
 }
 
-static mission_config_t *GetMissionForName(char *name)
+static mission_config_t *GetMissionForName(const char *name)
 {
     int i;
 
