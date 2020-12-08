379dc3210dc20d89e056583bde5beac5b363aa4a
Fabian Greffrath <fabian@greffrath.com>
2020-12-08 20:49:45 +0100

    bring back window height adjustment
    at the cost of the window shrinking when repeatedly changing widescreen.
    
    Rationale: I often do Win+Left or Win+Right to make the window half-fill
    my screen and align on the screen edge, and without this the game content
    wouldn't adjust to the new window size.

--- src/i_video.c
+++ src/i_video.c
@@ -315,9 +315,7 @@ static void AdjustWindowSize(void)
 {
     if (aspect_ratio_correct || integer_scaling)
     {
-        // [crispy] always adjust window width only, otherwise repeatedly
-        // changing widescreen settings causes the window to shrink.
-        if (window_width * actualheight <= window_height * SCREENWIDTH && false)
+        if (window_width * actualheight <= window_height * SCREENWIDTH)
         {
             // We round up window_height if the ratio is not exact; this leaves
             // the result stable.
