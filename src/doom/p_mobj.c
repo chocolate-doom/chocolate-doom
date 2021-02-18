8fd97779cec3d2f4d66f716e661f78f5e5ec24db
Fabian Greffrath <fabian@greffrath.com>
2021-02-18 13:25:09 +0100

    suppress interpolation of Archvile fire to mitigate it being spawned at the wrong location

--- src/doom/p_mobj.c
+++ src/doom/p_mobj.c
@@ -537,7 +537,8 @@ void P_MobjThinker (mobj_t* mobj)
 	return MusInfoThinker(mobj);
     }
     // [crispy] suppress interpolation of player missiles for the first tic
-    if (mobj->interp == -1)
+    // and Archvile fire to mitigate it being spawned at the wrong location
+    if (mobj->interp == -1 || mobj->type == MT_FIRE)
     {
         mobj->interp = false;
     }
