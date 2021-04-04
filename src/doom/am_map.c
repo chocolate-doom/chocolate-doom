21c7ac13b830e77cc32000bb20dbb6748b55ad4a
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2021-04-05 01:33:29 +0300

    Implement smooth automap rotation (#734)
    * Smooth automap rotation
    
    * Use const actualangle for smooth rotation

--- src/doom/am_map.c
+++ src/doom/am_map.c
@@ -1677,16 +1677,17 @@ AM_rotate
 static void AM_rotatePoint (mpoint_t *pt)
 {
     int64_t tmpx;
+    const angle_t actualangle = ANG90 - viewangle;
 
     pt->x -= mapcenter.x;
     pt->y -= mapcenter.y;
 
-    tmpx = (int64_t)FixedMul(pt->x, finecosine[mapangle>>ANGLETOFINESHIFT])
-         - (int64_t)FixedMul(pt->y, finesine[mapangle>>ANGLETOFINESHIFT])
+    tmpx = (int64_t)FixedMul(pt->x, finecosine[actualangle>>ANGLETOFINESHIFT])
+         - (int64_t)FixedMul(pt->y, finesine[actualangle>>ANGLETOFINESHIFT])
          + mapcenter.x;
 
-    pt->y = (int64_t)FixedMul(pt->x, finesine[mapangle>>ANGLETOFINESHIFT])
-          + (int64_t)FixedMul(pt->y, finecosine[mapangle>>ANGLETOFINESHIFT])
+    pt->y = (int64_t)FixedMul(pt->x, finesine[actualangle>>ANGLETOFINESHIFT])
+          + (int64_t)FixedMul(pt->y, finecosine[actualangle>>ANGLETOFINESHIFT])
           + mapcenter.y;
 
     pt->x = tmpx;
