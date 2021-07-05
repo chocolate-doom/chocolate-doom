d587245e4b5ac8ae6f2cfaf0ae73a9becd4cb459
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2021-07-05 23:37:43 +0300

    Implement smooth automap scrolling

--- src/doom/am_map.c
+++ src/doom/am_map.c
@@ -953,8 +953,10 @@ void AM_doFollowPlayer(void)
 
     if (f_oldloc.x != plr->mo->x || f_oldloc.y != plr->mo->y)
     {
-	m_x = FTOM(MTOF(plr->mo->x)) - m_w/2;
-	m_y = FTOM(MTOF(plr->mo->y)) - m_h/2;
+	// [JN] Use interpolated player coords for smooth
+	// scrolling and static player arrow position.
+	m_x = plr->mo->x - m_w/2;
+	m_y = plr->mo->y - m_h/2;
 	m_x2 = m_x + m_w;
 	m_y2 = m_y + m_h;
 	f_oldloc.x = plr->mo->x;
