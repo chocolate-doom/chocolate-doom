0371cab9e3515bd5986b42d4e6a0b03cb310231b
Fabian Greffrath <fabian@greffrath.com>
2021-09-06 07:54:19 +0200

    add REKKR to the list of recognized IWADs
    Fixes #760, thanks @Some1NamedNate

--- src/d_iwad.c
+++ src/d_iwad.c
@@ -43,6 +43,7 @@ static const iwad_t iwads[] =
     { "freedoom2.wad", doom2,    commercial, "Freedoom: Phase 2" },
     { "freedoom1.wad", doom,     retail,     "Freedoom: Phase 1" },
     { "freedm.wad",   doom2,     commercial, "FreeDM" },
+    { "rekkrsa.wad",  doom,      retail,     "REKKR" }, // [crispy] REKKR
     { "heretic.wad",  heretic,   retail,     "Heretic" },
     { "heretic1.wad", heretic,   shareware,  "Heretic Shareware" },
     { "hexen.wad",    hexen,     commercial, "Hexen" },
