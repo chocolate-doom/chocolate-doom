7e2d78deff5bad4db062cfc80074bc353c217dd1
Fabian Greffrath <fabian@greffrath.com>
2022-02-04 10:02:11 +0100

    quicksave to classic savegame slot, but skip questions (#832)
    * Revert "skip questions and save to dedicated quicksave slot (#818)"
    
    This reverts commit 47e80fd3a5ad0d8e7cde1686aa46e3869039ed2d.
    
    * quicksave to classic savegame slot, but skip questions
    
    * reset quickSaveSlot if it has been deleted

--- src/doom/m_menu.c
+++ src/doom/m_menu.c
@@ -88,7 +88,7 @@ int			screenblocks = 10; // [crispy] increased
 int			screenSize;
 
 // -1 = no quicksave slot picked!
-const int			quickSaveSlot=-1;
+int			quickSaveSlot;
 
  // 1 = message to be printed
 int			messageToPrint;
@@ -872,6 +872,10 @@ void M_LoadSelect(int choice)
     SaveDef.lastOn = choice;
     G_LoadGame (name);
     M_ClearMenus ();
+
+    // [crispy] allow quickload before quicksave
+    if (quickSaveSlot == -2)
+	quickSaveSlot = choice;
 }
 
 //
@@ -920,6 +924,10 @@ void M_DoSave(int slot)
 {
     G_SaveGame (slot,savegamestrings[slot]);
     M_ClearMenus ();
+
+    // PICK QUICKSAVE SLOT YET?
+    if (quickSaveSlot == -2)
+	quickSaveSlot = slot;
 }
 
 //
@@ -1033,6 +1041,15 @@ void M_SaveGame (int choice)
 //      M_QuickSave
 //
 
+void M_QuickSaveResponse(int key)
+{
+    if (key == key_menu_confirm)
+    {
+	M_DoSave(quickSaveSlot);
+	S_StartSoundOptional(NULL, sfx_mnucls, sfx_swtchx); // [NS] Optional menu sounds.
+    }
+}
+
 void M_QuickSave(void)
 {
     if (!usergame)
@@ -1043,9 +1060,16 @@ void M_QuickSave(void)
 
     if (gamestate != GS_LEVEL)
 	return;
-
-    G_QuickSaveGame();
-    S_StartSoundOptional(NULL, sfx_mnucls, sfx_swtchx); // [NS] Optional menu sounds.
+	
+    if (quickSaveSlot < 0)
+    {
+	M_StartControlPanel();
+	M_ReadSaveStrings();
+	M_SetupNextMenu(&SaveDef);
+	quickSaveSlot = -2;	// means to pick a slot now
+	return;
+    }
+    M_QuickSaveResponse(key_menu_confirm);
 }
 
 
@@ -1053,6 +1077,15 @@ void M_QuickSave(void)
 //
 // M_QuickLoad
 //
+void M_QuickLoadResponse(int key)
+{
+    if (key == key_menu_confirm)
+    {
+	M_LoadSelect(quickSaveSlot);
+	S_StartSoundOptional(NULL, sfx_mnucls, sfx_swtchx); // [NS] Optional menu sounds.
+    }
+}
+
 
 void M_QuickLoad(void)
 {
@@ -1062,9 +1095,17 @@ void M_QuickLoad(void)
 	M_StartMessage(DEH_String(QLOADNET),NULL,false);
 	return;
     }
-
-    M_LoadSelect(quickSaveSlot);
-    S_StartSoundOptional(NULL, sfx_mnucls, sfx_swtchx); // [NS] Optional menu sounds.
+	
+    if (quickSaveSlot < 0)
+    {
+	// [crispy] allow quickload before quicksave
+	M_StartControlPanel();
+	M_ReadSaveStrings();
+	M_SetupNextMenu(&LoadDef);
+	quickSaveSlot = -2;
+	return;
+    }
+    M_QuickLoadResponse(key_menu_confirm);
 }
 
 
@@ -1608,6 +1649,8 @@ void M_EndGameResponse(int key)
     if (demorecording || singledemo)
 	G_CheckDemoStatus();
 
+    // [crispy] clear quicksave slot
+    quickSaveSlot = -1;
     currentMenu->lastOn = itemOn;
     M_ClearMenus ();
     D_StartTitle ();
@@ -3001,6 +3044,7 @@ void M_Init (void)
     messageToPrint = 0;
     messageString = NULL;
     messageLastMenuActive = menuactive;
+    quickSaveSlot = -1;
 
     // Here we could catch other version dependencies,
     //  like HELP1/2, and four episodes.
@@ -3243,6 +3287,9 @@ static void M_ConfirmDeleteGameResponse (int key)
 
 		M_StringCopy(name, P_SaveGameFile(itemOn), sizeof(name));
 		remove(name);
+
+		if (itemOn == quickSaveSlot)
+			quickSaveSlot = -1;
 
 		M_ReadSaveStrings();
 	}
