29d7ed99072a547dc334f4568b160a5520562280
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2022-02-11 16:10:43 +0100

    Better input remapping

--- src/i_input.c
+++ src/i_input.c
@@ -317,6 +317,8 @@ static void UpdateMouseButtonState(unsigned int button, boolean on)
     // Note: button "0" is left, button "1" is right,
     // button "2" is middle for Doom.  This is different
     // to how SDL sees things.
+    // In the end: Left=0, Right=1, Middle=2,
+    // WheelUp=3 WheelDown=4, XButton1=5, XButton2=6
 
     switch (button)
     {
@@ -334,7 +336,9 @@ static void UpdateMouseButtonState(unsigned int button, boolean on)
 
         default:
             // SDL buttons are indexed from 1.
-            --button;
+            // And the wheel is mapped to button 3/4
+            // So we have to increment 2 and decrement 1 => inc 1.
+            button++;
             break;
     }
 
@@ -367,11 +371,11 @@ static void MapMouseWheelToButtons(SDL_MouseWheelEvent *wheel)
 
     if (wheel->y <= 0)
     {   // scroll down
-        button = 7;
+        button = 4;
     }
     else
     {   // scroll up
-        button = 6;
+        button = 3;
     }
 
     // post a button down event
