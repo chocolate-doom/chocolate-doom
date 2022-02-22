f689b40071ba650553c3b8e31988f1d5f4bfdc1c
Fabian Greffrath <fabian@greffrath.com>
2022-02-22 23:35:55 +0100

    add check for enough headroom to jumping condition
    Fixes #844

--- src/doom/p_user.c
+++ src/doom/p_user.c
@@ -375,7 +375,9 @@ void P_PlayerThink (player_t* player)
     // [crispy] jumping: apply vertical momentum
     if (cmd->arti)
     {
-        if ((cmd->arti & AFLAG_JUMP) && onground && !player->jumpTics)
+        if ((cmd->arti & AFLAG_JUMP) && onground &&
+            player->viewz < player->mo->ceilingz-16*FRACUNIT &&
+            !player->jumpTics)
         {
             // [crispy] Hexen sets 9; Strife adds 8
             player->mo->momz = (7 + crispy->jump) * FRACUNIT;
