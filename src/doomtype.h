bdcea20510773bf006557bd4e1b0d579a2848834
Fabian Greffrath <fabian@greffrath.com>
2022-03-21 09:26:03 +0100

    add trivial MIN/MAX macros to doomtype.h

--- src/doomtype.h
+++ src/doomtype.h
@@ -138,5 +138,12 @@ typedef int16_t dpixel_t;
 
 #define arrlen(array) (sizeof(array) / sizeof(*array))
 
+#ifndef MIN
+ #define MIN(a,b) (((a)<(b))?(a):(b))
+#endif
+#ifndef MAX
+ #define MAX(a,b) (((a)>(b))?(a):(b))
+#endif
+
 #endif
 
