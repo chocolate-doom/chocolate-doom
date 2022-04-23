dc0f1f5ce1cc7930678777681cfe31ea8564ed0f
Michael Day <contact@michaelcday.com>
2022-04-23 14:51:44 -0400

    Setup: Tweak mouse menu columns
    Give "Inventory right" a little extra room.

--- src/setup/mouse.c
+++ src/setup/mouse.c
@@ -98,7 +98,7 @@ static void ConfigExtraButtons(TXT_UNCAST_ARG(widget), TXT_UNCAST_ARG(unused))
                    buttons_table = TXT_NewTable(4),
                    NULL);
 
-    TXT_SetColumnWidths(buttons_table, 16, 11, 14, 10);
+    TXT_SetColumnWidths(buttons_table, 16, 11, 16, 10);
 
     AddMouseControl(buttons_table, "Move forward", &mousebforward);
     AddMouseControl(buttons_table, "Strafe left", &mousebstrafeleft);
