db16605af019515128abbc2874caaaad7a55c356
Turo Lamminen <turol@iki.fi>
2022-04-27 18:16:34 +0300

    textscreen: Fix use after free

--- textscreen/txt_window.c
+++ textscreen/txt_window.c
@@ -555,12 +555,12 @@ void TXT_OpenURL(const char *url)
 #endif
 
     retval = system(cmd);
-    free(cmd);
     if (retval != 0)
     {
         fprintf(stderr, "TXT_OpenURL: error executing '%s'; return code %d\n",
             cmd, retval);
     }
+    free(cmd);
 }
 
 #endif /* #ifndef _WIN32 */
