f6435a38c4516a21f440cdcf1c8b7ecec074aa97
Roman Fomin <rfomin@gmail.com>
2022-06-08 13:53:14 +0700

    set errno if conversion to UTF8 fails

--- src/m_misc.c
+++ src/m_misc.c
@@ -57,6 +57,7 @@ static wchar_t* ConvertToUtf8(const char *str)
 
     if (!wlen)
     {
+        errno = EINVAL;
         printf("Warning: Failed to convert path to UTF8\n");
         return NULL;
     }
@@ -71,6 +72,7 @@ static wchar_t* ConvertToUtf8(const char *str)
 
     if (MultiByteToWideChar(CP_UTF8, 0, str, -1, wstr, wlen) == 0)
     {
+        errno = EINVAL;
         printf("Warning: Failed to convert path to UTF8\n");
         free(wstr);
         return NULL;
