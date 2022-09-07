eaba328da879aa06b17d5fe0bcd3cd9a0bb05bcb
Fabian Greffrath <fabian@greffrath.com>
2022-09-07 06:42:12 +0200

    replace missing patches in textures with an actual dummy
    Choice fell on the HUD font's exclamation mark for some reason.
    
    This fixed loading of the REKKRSL.wad IWAD @SoDOOManiac.

--- src/doom/r_data.c
+++ src/doom/r_data.c
@@ -955,7 +955,7 @@ void R_InitTextures (void)
 		// [crispy] make non-fatal
 		fprintf (stderr, "R_InitTextures: Missing patch in texture %s\n",
 			 texturename);
-		patch->patch = 0;
+		patch->patch = W_CheckNumForName("STCFN033"); // [crispy] dummy patch
 	    }
 	}		
 	texturecolumnlump[i] = Z_Malloc (texture->width*sizeof(**texturecolumnlump), PU_STATIC,0);
