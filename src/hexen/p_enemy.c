1159572a1a18c196c8450cb1fcc8712d9eacb342
Turo Lamminen <turol@iki.fi>
2022-11-12 17:16:12 +0200

    hexen: Fix integer overflow in A_SerpentChase

--- src/hexen/p_enemy.c
+++ src/hexen/p_enemy.c
@@ -2028,7 +2028,7 @@ void A_SerpentChase(mobj_t *actor, player_t *player, pspdef_t *psp)
 //
     if (actor->movedir < 8)
     {
-        actor->angle &= (7 << 29);
+        actor->angle &= (7u << 29);
         delta = actor->angle - (actor->movedir << 29);
         if (delta > 0)
         {
