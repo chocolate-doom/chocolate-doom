355df5da80045ae859f7661f53dfaa29a338dc32
Turo Lamminen <turol@iki.fi>
2023-03-27 13:25:16 +0300

    Fix constness of windows music_win_module

--- src/i_winmusic.c
+++ src/i_winmusic.c
@@ -1496,7 +1496,7 @@ static boolean I_WIN_MusicIsPlaying(void)
     return (song.num_tracks > 0);
 }
 
-static snddevice_t music_win_devices[] =
+static const snddevice_t music_win_devices[] =
 {
     SNDDEVICE_PAS,
     SNDDEVICE_WAVEBLASTER,
@@ -1505,7 +1505,7 @@ static snddevice_t music_win_devices[] =
     SNDDEVICE_AWE32,
 };
 
-music_module_t music_win_module =
+const music_module_t music_win_module =
 {
     music_win_devices,
     arrlen(music_win_devices),
