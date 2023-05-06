9e0fcb0c9aa4076007c0acbe950b00fe59aea242
Julia Nechaevskaya <julia.nechaevskaya@live.com>
2023-05-06 07:44:04 +0300

    Fix compiler warning

--- src/doom/r_things.c
+++ src/doom/r_things.c
@@ -782,7 +782,7 @@ void R_SortVisSprites (void)
     int			count;
     vissprite_t*	ds;
     vissprite_t*	best;
-    vissprite_t		unsorted;
+    static vissprite_t		unsorted;
     fixed_t		bestscale;
 
     count = vissprite_p - vissprites;
