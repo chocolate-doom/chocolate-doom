f2b575c08d4cbeefdbb264f33541b21bd2dc4d2c
Fabian Greffrath <fabian@greffrath.com>
2023-08-31 12:37:06 +0200

    properly check if a patch in patchlookup[] is actually a patch
    Fixes #1093

--- src/doom/r_data.c
+++ src/doom/r_data.c
@@ -169,6 +169,59 @@ fixed_t*	spritetopoffset;
 
 lighttable_t	*colormaps;
 
+// [FG] check if the lump can be a Doom patch
+// taken from PrBoom+ prboom2/src/r_patch.c:L350-L390
+
+static boolean R_IsPatchLump (const int lump)
+{
+  int size;
+  int width, height;
+  const patch_t *patch;
+  boolean result;
+
+  if (lump < 0)
+    return false;
+
+  size = W_LumpLength(lump);
+
+  // minimum length of a valid Doom patch
+  if (size < 13)
+    return false;
+
+  patch = (const patch_t *)W_CacheLumpNum(lump, PU_CACHE);
+
+  // [FG] detect patches in PNG format early
+  if (!memcmp(patch, "\211PNG\r\n\032\n", 8))
+    return false;
+
+  width = SHORT(patch->width);
+  height = SHORT(patch->height);
+
+  result = (height > 0 && height <= 16384 && width > 0 && width <= 16384 && width < size / 4);
+
+  if (result)
+  {
+    // The dimensions seem like they might be valid for a patch, so
+    // check the column directory for extra security. All columns
+    // must begin after the column directory, and none of them must
+    // point past the end of the patch.
+    int x;
+
+    for (x = 0; x < width; x++)
+    {
+      unsigned int ofs = LONG(patch->columnofs[x]);
+
+      // Need one byte for an empty column (but there's patches that don't know that!)
+      if (ofs < (unsigned int)width * 4 + 8 || ofs >= (unsigned int)size)
+      {
+        result = false;
+        break;
+      }
+    }
+  }
+
+  return result;
+}
 
 //
 // MAPTEXTURE_T CACHING
@@ -452,17 +505,6 @@ void R_GenerateLookup (int texnum)
 	x1 = patch->originx;
 	x2 = x1 + SHORT(realpatch->width);
 
-	// [crispy] detect patches in PNG format... and fail
-	{
-		const unsigned char *magic = (const unsigned char *) realpatch;
-
-		if (magic[0] == 0x89 &&
-		    magic[1] == 'P' && magic[2] == 'N' && magic[3] == 'G')
-		{
-			I_Error("Patch in PNG format detected: %.8s", lumpinfo[patch->patch]->name);
-		}
-	}
-	
 	if (x1 < 0)
 	    x = 0;
 	else
@@ -807,17 +849,14 @@ void R_InitTextures (void)
     {
 	for (j = 0; j < pnameslumps[i].nummappatches; j++)
 	{
-	    int p, po;
+	    int p;
 
 	    M_StringCopy(name, pnameslumps[i].name_p + j * 8, sizeof(name));
-	    p = po = W_CheckNumForName(name);
-	    // [crispy] prevent flat lumps from being mistaken as patches
-	    while (p >= firstflat && p <= lastflat)
-	    {
-		p = W_CheckNumForNameFromTo (name, p - 1, 0);
-	    }
+	    p = W_CheckNumForName(name);
+	    if (!R_IsPatchLump(p))
+	        p = -1;
 	    // [crispy] if the name is unambiguous, use the lump we found
-	    patchlookup[k++] = (p == -1) ? po : p;
+	    patchlookup[k++] = p;
 	}
     }
 
