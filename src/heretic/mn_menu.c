chocolate-doom/chocolate-doom

commit cbd56ecdba31ad08dc7269fcda792b79d8dcc034

Heretic & Hexen: demo playback fixes (#1151)
* Heretic/Hexen: enable demo fast-forwarding
The button was already assignable, it just didn't do anything.
Additionally, don't pop up the menu during a demo, unless the user
presses the menu key, as in Crispy Doom.

* Hexen: fix usergame being disabled when not in a demo

src/heretic/mn_menu.c
@@ -2220,7 +2220,8 @@ boolean MN_Responder(event_t * event)
 
     if (!MenuActive)
     {
-        if (key == key_menu_activate || gamestate == GS_DEMOSCREEN || demoplayback)
+        // [crispy] don't pop up the menu on other keys during a demo
+        if (key == key_menu_activate) //|| gamestate == GS_DEMOSCREEN || demoplayback)
         {
             MN_ActivateMenu();
             return (true);

src/hexen/mn_menu.c
@@ -2215,7 +2215,8 @@ boolean MN_Responder(event_t * event)
 
     if (!MenuActive)
     {
-        if (key == key_menu_activate || gamestate == GS_DEMOSCREEN || demoplayback)
+        // [crispy] don't pop up the menu on other keys during a demo
+        if (key == key_menu_activate) //|| gamestate == GS_DEMOSCREEN || demoplayback)
         {
             MN_ActivateMenu();
             return (true);

src/heretic/g_game.c
@@ -1142,6 +1142,14 @@ boolean G_Responder(event_t * ev)
         usearti = true;
     }
 
+    // [crispy] demo fast-forward
+    if (ev->type == ev_keydown && ev->data1 == key_demospeed && 
+        (demoplayback || gamestate == GS_DEMOSCREEN))
+    {
+        singletics = !singletics;
+        return true;
+    }
+
     // Check for spy mode player cycle
     if (gamestate == GS_LEVEL && ev->type == ev_keydown
         && ev->data1 == KEY_F12 && !deathmatch)

src/hexen/g_game.c
@@ -1072,6 +1072,14 @@ boolean G_Responder(event_t * ev)
         usearti = true;
     }
 
+    // [crispy] demo fast-forward
+    if (ev->type == ev_keydown && ev->data1 == key_demospeed && 
+        (demoplayback || gamestate == GS_DEMOSCREEN))
+    {
+        singletics = !singletics;
+        return true;
+    }
+
     // Check for spy mode player cycle
     if (gamestate == GS_LEVEL && ev->type == ev_keydown
         && ev->data1 == key_spy && !deathmatch)
@@ -2049,7 +2057,9 @@ void G_InitNew(skill_t skill, int episode, int map)
     }
 
     // Set up a bunch of globals
-    if (!demoextend)
+    // [crispy] since demoextend is the default, we also want to check to
+    // make sure we're not playing a demo
+    if (!demoextend || (!demorecording && !demoplayback))
     {
         // This prevents map-loading from interrupting a demo.
         // demoextend is set back to false only if starting a new game or

commit 2ec5e461ebf16dbaa3bd309d816ad0beecd46de3

Heretic & Hexen: any key opens the menu when not in a demo Fixes minor regression in #1151 Thanks @mikeday0

src/heretic/mn_menu.c
@@ -2221,7 +2221,7 @@ boolean MN_Responder(event_t * event)
     if (!MenuActive)
     {
         // [crispy] don't pop up the menu on other keys during a demo
-        if (key == key_menu_activate) //|| gamestate == GS_DEMOSCREEN || demoplayback)
+        if (key == key_menu_activate || gamestate == GS_DEMOSCREEN || (demoplayback && !singledemo))
         {
             MN_ActivateMenu();
             return (true);

src/hexen/mn_menu.c
@@ -2225,7 +2225,7 @@ boolean MN_Responder(event_t * event)
     if (!MenuActive)
     {
         // [crispy] don't pop up the menu on other keys during a demo
-        if (key == key_menu_activate) //|| gamestate == GS_DEMOSCREEN || demoplayback)
+        if (key == key_menu_activate || gamestate == GS_DEMOSCREEN || (demoplayback && !singledemo))
         {
             MN_ActivateMenu();
             return (true);