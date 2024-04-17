2ea764fed6a266e291cf37375f52bc92260f686c
Henrique194 <henriquejb194@gmail.com>
2024-04-17 12:10:45 -0300

    doom/strife: Fix possible UB in function P_SpawnMapThing when executing left shift with negative gameskill

--- src/doom/p_mobj.c
+++ src/doom/p_mobj.c
@@ -804,7 +804,11 @@ void P_SpawnMapThing (mapthing_t* mthing)
     else if (gameskill == sk_nightmare)
 	bit = 4;
     else
-	bit = 1<<(gameskill-1);
+        // avoid undefined behavior (left shift by negative value and rhs too big)
+        // by accurately emulating what doom.exe did: reduce mod 32.
+        // For more details check:
+        // https://github.com/chocolate-doom/chocolate-doom/issues/1677
+        bit = (int) (1U << ((gameskill - 1) & 0x1F));
 
     if (!(mthing->options & bit) )
 	return;
