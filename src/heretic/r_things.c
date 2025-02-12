8fda40abfc2da16d51a2ba721443fba2d6b9c66f
Noseey <omega_1986@hotmail.com>
2025-02-12 19:24:23 +0100

    Heretic: Fix Translucency Option affecting MT_MUMMYLEADERGHOST (#1273)
    * Heretic: Checking for doomednum to draw base-frame.
    
    * Heretic: Add comment MT_MUMMYLEADER to r_things.c
    
    * Heretic: Rework check for MT_Mummyleader base drawing.

--- src/heretic/r_things.c
+++ src/heretic/r_things.c
@@ -753,10 +753,10 @@ void R_AddSprites(sector_t * sec)
 
     for (thing = sec->thinglist; thing; thing = thing->snext)
     {
-        // [crispy] draw base frame and translucent current frame for mummyleader attack
+        // [crispy] draw base frame and translucent current frame for MT_MUMMYLEADER attack
         if (crispy->translucency & TRANSLUCENCY_MISSILE)
         {
-            if (thing->state->sprite == SPR_MUMM && thing->frame == (24 | FF_FULLBRIGHT))
+            if (thing->state->sprite == SPR_MUMM && !(thing->flags & MF_SHADOW) && thing->frame == (24 | FF_FULLBRIGHT))
             {
                 thing->frame = (thing->frame & FF_FRAMEMASK) - 1; // [crispy] set attack stance without fire
                 R_ProjectSprite(thing);
@@ -765,7 +765,7 @@ void R_AddSprites(sector_t * sec)
             }
         }
         R_ProjectSprite(thing);
-        if (thing->state->sprite == SPR_MUMM && thing->flags & MF_TRANSLUCENT)
+        if (thing->state->sprite == SPR_MUMM && !(thing->flags & MF_SHADOW) && thing->flags & MF_TRANSLUCENT)
         {
             thing->flags &= ~MF_TRANSLUCENT;
         }
